98a42aa256ff7a94907703001db2b0035f583c7c
Farhan Rashad <farhan@dynexcel.com>
2019-12-29 02:32:03 +0500

    add partner fields

---
+++ de_partner_extra_fields/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Partner extra Field",
+
+    'summary': """
+        Add more fields 
+        - NTN Number
+        - CNIC
+        """,
+
+    'description': """
+        Add extra fields in partner page
+    """,
+
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
