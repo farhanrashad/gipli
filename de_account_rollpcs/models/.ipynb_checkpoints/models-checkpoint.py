74592ee3fd5983edc5298cdcc4d6f31a4695dbb2
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-08-12 09:30:10 +0000

    roll pcs updated

--- de_account_rollpcs/models/.ipynb_checkpoints/models-checkpoint.py
+++ de_account_rollpcs/models/.ipynb_checkpoints/models-checkpoint.py
@@ -8,3 +8,10 @@ class AccountMoveLine(models.Model):
 
     roll = fields.Char(string='Roll')
     pcs = fields.Char(string='PCS')
+    weight_price = fields.Float(string='Weight Price', domain=[('parnter_id', 'in', 'CONE')])
+    subtotal_weight = fields.Monetary(compute='_compute_amount_t', string='Subtotal')
+    
+    @api.depends('subtotal_weight','weight_price', 'demand_qty')    
+    def _compute_amount_t(self):
+        for line in self:
+            line.subtotal_weight = line.weight_price * line.demand_qty
