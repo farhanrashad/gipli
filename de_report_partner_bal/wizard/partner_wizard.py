8515f0a4ef4d4f7acfe6da47d0319b70e429296e
Hamayunfazal <hamayunfazal1@gmail.com>
2022-05-23 08:26:16 +0000

    updated

--- de_report_partner_bal/wizard/partner_wizard.py
+++ de_report_partner_bal/wizard/partner_wizard.py
@@ -9,17 +9,17 @@ class PartnerWizard(models.TransientModel):
     
     date = fields.Date(string='Date')
         
-    partner_ids = fields.Many2many('res.partner')
+#     partner_ids = fields.Many2many('res.partner')
     
     
     
     def print_report(self):
         data = {}
-        data['form'] = self.read(['date', 'partner_ids'])[0]
+        data['form'] = self.read(['date'])[0]
         return self._print_report(data)
     
     def _print_report(self, data):
-        data['form'].update(self.read(['date', 'partner_ids'])[0])
+        data['form'].update(self.read(['date'])[0])
         return self.env.ref('de_report_partner_bal.partner_bal_report_xlsx').report_action(self, data=data, config=False)
     
     
