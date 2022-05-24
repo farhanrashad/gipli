6792a161a6576e29b4f1ea3a2c7dbd7bcace156c
hamayun-dyn <91132653+hamayunfazal@users.noreply.github.com>
2022-05-24 16:26:04 +0500

    commit

--- de_add_report_to_sale_invoice/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-    
-    
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    strg = fields.Char(string="Sales Tax reg Number:")   
--- de_invoice_multi_products_selection/wizard/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import multiple_products_invoice
