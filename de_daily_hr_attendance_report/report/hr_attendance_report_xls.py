d8f2ba17f4c91a35c91a9417a77202a070827e70
hamayunfazal <hamayunfazal1@gmail.com>
2022-06-13 20:09:41 +0000

    updated

--- de_daily_hr_attendance_report/report/hr_attendance_report_xls.py
+++ de_daily_hr_attendance_report/report/hr_attendance_report_xls.py
@@ -22,15 +22,15 @@ class EmployeeAttendanceXlS(models.AbstractModel):
         sheet = workbook.add_worksheet('Employee Attendance Report')
         bold = workbook.add_format({'bold': True, 'align': 'center', 'border': True})
         
-        sheet.write('C2:C2', 'Employee Attendance', bold)
-        sheet.write('C3:C3', 'daily: '+ data.on_date .strftime('%d-%b-%Y'), bold)
+        sheet.write('D2:D2', 'Employee Attendance', bold)
+        sheet.write('D3:D3', 'daily: '+ data.on_date .strftime('%d-%b-%Y'), bold)
         
         
         
         sheet.set_column('A:B', 20, )
-        sheet.set_column('C:C', 30, )
-        sheet.set_column('D:D', 20, )
-        sheet.set_column('E:F', 20, )
+        sheet.set_column('C:C', 20, )
+        sheet.set_column('D:D', 30, )
+        sheet.set_column('E:F', 30, )
         sheet.set_column('G:H', 20, )
         sheet.set_column('I:J', 20, )
         
@@ -44,7 +44,7 @@ class EmployeeAttendanceXlS(models.AbstractModel):
         sheet.write(5, 6, 'Absent', bold)
         
         
-        row = 7
+        
         sr_no = 0
         
     
@@ -74,38 +74,90 @@ class EmployeeAttendanceXlS(models.AbstractModel):
 #             for day in range(31):
 #                 date_list.append(start_of_week - datetime.timedelta(days=day))
 
-            sr_no += 1
+        sr_no += 1
+        row = 7
+        row_out = 7
         if data.all_employees:
-            employees = self.env['hr.employee'].search([]).ids
+            employees = self.env['hr.employee'].search([])
         if data.employee:
-            employees = self.env['hr.employee'].search([('id','=', data.employee.id)]).ids
+            employees = self.env['hr.employee'].search([('id','=', data.employee.id)])
             
-        hr_attendance = self.env['hr.attendance'].search([('attendance_date','in', date_list),('employee_id', 'in', employees)])
+        
+        for l in employees:
+            hr_attendance = self.env['hr.attendance'].search([('attendance_date','in', date_list),('employee_id', '=', l.id)])
+            sheet.write(row_out, 1, l.name, format1) 
+            sheet.write(row, 0, sr_no, format1)
+
         
        
-        for line in hr_attendance:
-            sheet.write(row, 0, sr_no, format1)
-            sheet.write(row, 1, line.employee_id.name, format1)
-            if line.check_in and line.check_out:
-                sheet.write(row, 2, 'P', format1)
-                sheet.write(row, 5, '1', format1)
-                sheet.write(row, 6, '0', format1)
+            if hr_attendance:
+                for line in hr_attendance:
+                    
+                    if line.check_in and line.check_out:
+                        sheet.write(row, 2, 'P', format1)
+                        sheet.write(row, 5, '1', format1)
+                        sheet.write(row, 6, '0', format1)
+                    else:
+                        sheet.write(row, 2, 'A', format1)
+                        sheet.write(row, 5, '0', format1)
+                        sheet.write(row, 6, '1', format1)
+                    sheet.write(row, 3, line.check_in.strftime('%d/%m/%Y %H:%M:%S'), format1)
+                    if line.check_out:
+                        sheet.write(row, 4, line.check_out.strftime('%d-%m-%Y %H:%M:%S'), format1)
+                    row = row+1
             else:
-                sheet.write(row, 2, 'A', format1)
-                sheet.write(row, 5, '0', format1)
-                sheet.write(row, 6, '1', format1)
-            sheet.write(row, 3, line.check_in.strftime('%d/%m/%Y %H:%M:%S'), format1)
-            if line.check_out:
-                sheet.write(row, 4, line.check_out.strftime('%d-%m-%Y %H:%M:%S'), format1)
-                
-            
-            row = row+1
-            sr_no += 1
-            
-            
+                att_dates = self.env['hr.attendance'].search([('attendance_date','in', date_list)])
+                for date in att_dates:
+                    sheet.write(row, 2, 'A', format1)
+                    sheet.write(row, 5, '0', format1)
+                    sheet.write(row, 6, '1', format1)
+                    sheet.write(row, 3, '--', format1)
+                    sheet.write(row, 4, '--', format1)
 
-#                 sheet.write(row, 4, total_hours, format1)
-#             +
+                    row = row+1
+            sr_no += 1
+                
+            row_out = row
+            
+            
+            
+#         result = []
+#         result2 = []
+#         for emp in employees:
+#             main_dict = {'sno':sr_no,'emp_name':emp.name,'p_count':'','a_count':'','att_dict':''}
+#             emp_attendance = self.env['hr.attendance'].search([('attendance_date','in', date_list),('employee_id', '=', emp.id)])
+#             print("emp and employee attendance ++++++++++++++++ ",emp,emp_attendance)
+            
+#             for att in emp_attendance:
+#                 att_dict = {'check_in':'', 'check_out':'', 'status':'', }
+#                 if att.check_in and att.check_out:
+#                     att_dict["check_in"] = att.check_in.strftime('%d/%m/%Y %H:%M:%S')
+#                     att_dict["check_out"] = att.check_out.strftime('%d-%m-%Y %H:%M:%S')
+#                     att_dict["status"] = 'P'
+#                     att_dict["p_count"] = 1
+#                     att_dict["a_count"] = 0
+#                 else:
+#                     att_dict["check_in"] = '--'
+#                     att_dict["check_out"] = '--'
+#                     att_dict["status"] = 'A'
+                
+#                 result2.append(att_dict)
+#                 main_dict['att_dict'] = result2
+#             result.append(main_dict)
+#             sr_no += 1
+        
+#         row_out = 7
+#         row_in = 7
+#         for rec in result:
+#             sheet.write(row_out, 0, rec['sno'], format1)
+#             sheet.write(row_out, 1, rec['emp_name'], format1)            
+#             for data in rec['att_dict']:
+#                 sheet.write(row_in, 2, data['check_in'], format1)
+#                 sheet.write(row_in, 3, data['check_out'], format1)
+#                 sheet.write(row_in, 4, data['status'], format1)
+#                 row_in +=1
+#             row_out = row_in + 1
+
             
         
         
