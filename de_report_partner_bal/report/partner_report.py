5e3f96ad89747f476de9a82d10b59c7ffee812a8
Mehdihaider110 <mehdi.haider@e-bizsoft.com>
2023-01-09 21:38:55 +0500

    add walking Customer column in report

--- de_report_partner_bal/report/partner_report.py
+++ de_report_partner_bal/report/partner_report.py
@@ -11,26 +11,24 @@ class PartnerBalanceXlS(models.AbstractModel):
     _name = 'report.de_report_partner_bal.partner_bal_report_xlsx'
     _description = 'Partner Balence Xlsx report'
     _inherit = 'report.report_xlsx.abstract'
-    
-  
+
     def generate_xlsx_report(self, workbook, data, lines):
         data = self.env["partner.wizard"].browse(self.env.context.get('active_ids'))
         format1 = workbook.add_format({'font_size': '12', 'align': 'center', 'bold': False})
         sheet = workbook.add_worksheet('Employee Attendance Report')
         bold = workbook.add_format({'bold': True, 'align': 'center', 'border': True})
-        
-       
-        sheet.write('C1:C1', data.date.strftime('%d-%m-%Y'), bold)
-        
-        
-        format0 = workbook.add_format({'font_size': '12', 'align': 'vcenter', 'bold': True,})
-        format1 = workbook.add_format({'font_size': '12', 'align': 'vcenter', 'bold': True, 'bg_color': 'yellow'})       
-        label =workbook.add_format({'font_size': '10', 'align': 'center', 'bold': True, 'border':True, 'bg_color': 
-                                    '#CCFFFF'})
-        format_txt = workbook.add_format({'font_size': '10', 'align': 'center', 'border':True,})
-        format_num = workbook.add_format({'font_size': '10', 'align': 'right', 'num_format':'#,##0.00', 'border':True})
-                                                                                                                                                      
-        
+        date_format = workbook.add_format({'bold': True, 'align': 'center', 'border': True, 'num_format': 'dd/mm/yy'})
+
+        sheet.write('C1:C1', data.date, date_format)
+
+        format0 = workbook.add_format({'font_size': '12', 'align': 'vcenter', 'bold': True, })
+        format1 = workbook.add_format({'font_size': '12', 'align': 'vcenter', 'bold': True, 'bg_color': 'yellow'})
+        label = workbook.add_format({'font_size': '10', 'align': 'center', 'bold': True, 'border': True, 'bg_color':
+            '#CCFFFF'})
+        format_txt = workbook.add_format({'font_size': '10', 'align': 'center', 'border': True, })
+        format_num = workbook.add_format(
+            {'font_size': '10', 'align': 'right', 'num_format': '#,##0.00', 'border': True})
+
         sheet.set_column(0, 0, 20)
         sheet.set_column(1, 1, 50)
         sheet.set_column(2, 2, 40)
@@ -40,78 +38,90 @@ class PartnerBalanceXlS(models.AbstractModel):
         sheet.set_column(6, 6, 20)
         row = 3
         col = 0
-        
-       
-        
-        sheet.write(2,0,'Code', bold)
-        sheet.write(2,1 , 'Name',bold)
-        sheet.write(2,2 , 'Email',bold)
-        sheet.write(2,3 , 'City',bold)
-        sheet.write(2,4 , 'CNIC',bold)
-        sheet.write(2,5 , 'NTN',bold)
-        sheet.write(2,6 , 'STRN',bold)
-        sheet.write(2,7 , 'Balance',bold)
 
-       
-#         entery_type = ''
-#         if data['state'] == 'draft':
-#             entery_type == "and parent_state = " + data['draft']
-#         elif data['state'] == 'draft':
-#             entery_type == "and parent_state = " + data['']
-#             raise UserError("test")
-           
-            
-        company_id = ''  
+        sheet.write(2, 0, 'Code', bold)
+        sheet.write(2, 1, 'Name', bold)
+        if data.account_type == 'receivable':
+            sheet.write(2, 2, 'Walking Customer', bold)
+            sheet.write(2, 3, 'Email', bold)
+            sheet.write(2, 4, 'City', bold)
+            sheet.write(2, 5, 'CNIC', bold)
+            sheet.write(2, 6, 'NTN', bold)
+            sheet.write(2, 7, 'STRN', bold)
+            sheet.write(2, 8, 'Balance', bold)
+        elif data.account_type == 'payable':
+            sheet.write(2, 2, 'Email', bold)
+            sheet.write(2, 3, 'City', bold)
+            sheet.write(2, 4, 'CNIC', bold)
+            sheet.write(2, 5, 'NTN', bold)
+            sheet.write(2, 6, 'STRN', bold)
+            sheet.write(2, 7, 'Balance', bold)
+
+        #         entery_type = ''
+        #         if data['state'] == 'draft':
+        #             entery_type == "and parent_state = " + data['draft']
+        #         elif data['state'] == 'draft':
+        #             entery_type == "and parent_state = " + data['']
+        #             raise UserError("test")
+
+        company_id = ''
         if data['date']:
             date_param = " to_char(date) <= " + data['date'].strftime("%Y-%m-%d")
-        company_id = data.company_id.id            
+        company_id = data.company_id.id
         dated = data['date'].strftime("%Y-%m-%d")
-#         current_date=str((data.date))
-#         sssssssss = datetime.strptime(current_date, "%Y-%m-%d").strftime("%d/%m/%Y")
+
+        #         current_date=str((data.date))
+        #         sssssssss = datetime.strptime(current_date, "%Y-%m-%d").strftime("%d/%m/%Y")
         # -------------------------------------------------------------------------
         # Query for lease agreement
         # -------------------------------------------------------------------------
-        
+
         if data.account_type == 'payable':
             self._cr.execute("""
-                select p.ref, p.name, p.vat as strn, p.ntn, p.email, p.nic, p.city, sum(l.debit) - sum(l.credit) as bal
-                from account_move_line l
-                join account_account a on l.account_id = a.id
-                join account_move m on l.move_id = m.id
-                join res_partner p on l.partner_id = p.id
-                where  a.internal_type = '""" + str(data.account_type) +  """' and l.date <= '""" + str(dated) +  """' and l.company_id='"""+ str(company_id)  + """'
-                and m.state in ('draft', 'posted')
-                group by p.ref, p.name, p.vat, p.email, p.ntn, p.nic, p.city
-                """)
+                   select p.ref, p.name, p.vat as strn, p.ntn, p.email, p.nic, p.city, sum(l.debit) - sum(l.credit) as bal
+                   from account_move_line l
+                   join account_account a on l.account_id = a.id
+                   join account_move m on l.move_id = m.id
+                   join res_partner p on l.partner_id = p.id
+                   where  a.internal_type = '""" + str(data.account_type) + """' and l.date <= '""" + str(
+                dated) + """' and l.company_id='""" + str(company_id) + """'
+                   and m.state in ('draft', 'posted')
+                   group by p.ref, p.name, p.vat, p.email, p.ntn, p.nic, p.city
+                   """)
         if data.account_type == 'receivable':
-             self._cr.execute("""
-                select p.ref, p.name, p.vat as strn, p.ntn, p.email, p.nic, p.city, sum(l.debit) - sum(l.credit) as bal
-                from account_move_line l
-                join account_account a on l.account_id = a.id
-                join account_move m on l.move_id = m.id
-                join res_partner p on l.partner_id = p.id
-                where a.internal_type = '""" + str(data.account_type) +  """' and l.date <= '""" + str(dated) +  """' and l.company_id='"""+ str(company_id)  + """'
-                and m.state in ('draft', 'posted')
-                group by p.ref, p.name, p.vat, p.email, p.ntn, p.nic, p.city
-                """)
-            
-        
-        
-                         
-        rs_move = self._cr.dictfetchall()
-        
-        for move in rs_move:
-            sheet.write(row,0, move['ref'], format_txt)
-            sheet.write(row,1, move['name'], format_txt)
-            sheet.write(row,2, move['email'], format_txt)
-            sheet.write(row,3, move['city'], format_txt)
-            sheet.write(row,4, move['nic'], format_txt)
-            sheet.write(row,5, move['ntn'], format_txt)
-            sheet.write(row,6, move['strn'], format_txt)
-            sheet.write(row,7, move['bal'], format_num)
-            
+            self._cr.execute("""
+                   select p.ref, p.name,p.wac_ref as walking_customer,p.vat as strn, p.ntn, p.email, p.nic, p.city, sum(l.debit) - sum(l.credit) as bal
+                   from account_move_line l
+                   join account_account a on l.account_id = a.id
+                   join account_move m on l.move_id = m.id
+                   join res_partner p on l.partner_id = p.id
+                   where a.internal_type = '""" + str(data.account_type) + """' and l.date <= '""" + str(
+                dated) + """' and l.company_id='""" + str(company_id) + """'
+                   and m.state in ('draft', 'posted')
+                   group by p.ref, p.name,p.wac_ref, p.vat, p.email, p.ntn, p.nic, p.city
+                   """)
 
-             
-                
+        rs_move = self._cr.dictfetchall()
+
+        for move in rs_move:
+            sheet.write(row, 0, move['ref'], format_txt)
+            sheet.write(row, 1, move['name'], format_txt)
+            if data.account_type == 'receivable':
+                sheet.write(row, 2, move['walking_customer'], format_txt)
+                sheet.write(row, 3, move['email'], format_txt)
+                sheet.write(row, 4, move['city'], format_txt)
+                sheet.write(row, 5, move['nic'], format_txt)
+                sheet.write(row, 6, move['ntn'], format_txt)
+                sheet.write(row, 7, move['strn'], format_txt)
+                sheet.write(row, 8, move['bal'], format_num)
+            elif data.account_type == 'payable':
+                sheet.write(row, 2, move['email'], format_txt)
+                sheet.write(row, 3, move['city'], format_txt)
+                sheet.write(row, 4, move['nic'], format_txt)
+                sheet.write(row, 5, move['ntn'], format_txt)
+                sheet.write(row, 6, move['strn'], format_txt)
+                sheet.write(row, 7, move['bal'], format_num)
+
             row += 1
+
         
