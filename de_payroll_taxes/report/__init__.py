25fbb61c5a5eb5073a8c1c431e485bb2c90b8ca2
Farhan Rashad <farhan@dynexcel.com>
2023-06-03 18:58:06 +0630

    remove modules

--- de_payroll_taxes/report/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import hr_tax_credit_report
-from . import payment_request_report
--- de_payslip_batches_comparison/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import payslip_batch_wise_wiz
-
-
