48bfa491cf54a06fcc6627d749ece63b9bfb3fd7
farhan <farhan@dynexcel.com>
2023-09-09 08:39:50 +0000

    update portal hr service

--- de_portal_hr_service/controllers/portal.py
+++ de_portal_hr_service/controllers/portal.py
@@ -408,21 +408,19 @@ class CustomerPortal(portal.CustomerPortal):
 
             # default values for header model
             for field in service_id.header_model_id.field_id.filtered(lambda r: r.relation and r.ttype == 'many2one' and r.store):
-                try:
+                if not kw.get(field.field_name):
                     if field.relation == 'res.users':
                         vals.update({
                             field.name: user_id.id,
                         })
-                    elif field.relation == 'hr.employee':
+                    if field.relation == 'hr.employee':
                         vals.update({
                             field.name: user_id.employee_id.id,
                         })
-                    elif field.relation == 'res.partner':
+                    if field.relation == 'res.partner':
                         vals.update({
                             field.name: user_id.partner_id.id,
                         })
-                except:
-                    pass
         elif line_item.line_model_id: #service_id.line_model_id.id == int(model_id):
             service_items = line_item.hr_service_record_line_items #line_model_id.hr_service_record_line_items #.hr_service_items_line
             if edit_mode == '1' or edit_mode == 1:
@@ -430,21 +428,19 @@ class CustomerPortal(portal.CustomerPortal):
                 res_name = record_sudo.name
                 # default values for header model
             for field in line_item.line_model_id.field_id.filtered(lambda r: r.relation and r.ttype == 'many2one' and r.store):
-                try:
+                if not kw.get(field.field_name):
                     if field.relation == 'res.users':
                         vals.update({
                             field.name: user_id.id,
                         })
-                    elif field.relation == 'hr.employee':
+                    if field.relation == 'hr.employee':
                         vals.update({
                             field.name: user_id.employee_id.id,
                         })
-                    elif field.relation == 'res.partner':
+                    if field.relation == 'res.partner':
                         vals.update({
                             field.name: user_id.partner_id.id,
                         })
-                except:
-                    pass
         
         
         for field in service_items:
