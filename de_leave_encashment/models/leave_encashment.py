ff8305917c41d80b5239c0074a46c7ce12675498
farhan <farhan@dynexcel.com>
2023-09-27 14:58:06 +0000

    update encahsment

--- de_leave_encashment/models/leave_encashment.py
+++ de_leave_encashment/models/leave_encashment.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, api, _
-from odoo.exceptions import UserError
+from odoo.exceptions import AccessError, UserError, ValidationError
+from datetime import datetime, timedelta, time
+from pytz import timezone, UTC
+from odoo.tools import date_utils
+from odoo.addons.base.models.res_partner import _tz_get
 
 
 class LeaveEncashment(models.Model):
@@ -52,6 +56,17 @@ class LeaveEncashment(models.Model):
         domain="[('company_id', '=?', employee_company_id), '|', ('requires_allocation', '=', 'no'), ('has_valid_allocation', '=', True)]", tracking=True)
     leave_type_request_unit = fields.Selection(related='holiday_status_id.request_unit', readonly=True)
     validation_type = fields.Selection(string='Validation Type', related='holiday_status_id.leave_validation_type', readonly=False)
+
+    active = fields.Boolean(default=True, readonly=True)
+    first_approver_id = fields.Many2one(
+        'hr.employee', string='First Approval', readonly=True, copy=False,
+        help='This area is automatically filled by the user who validate the time off')
+    second_approver_id = fields.Many2one(
+        'hr.employee', string='Second Approval', readonly=True, copy=False,
+        help='This area is automatically filled by the user who validate the time off with second level (If time off type need second validation)')
+    can_reset = fields.Boolean('Can reset', compute='_compute_can_reset')
+    can_approve = fields.Boolean('Can Approve', compute='_compute_can_approve')
+    can_cancel = fields.Boolean('Can Cancel', compute='_compute_can_cancel')
     
     department_id = fields.Many2one('hr.department', string='Department', compute='_compute_from_employee_id')
     job_id = fields.Many2one('hr.job', string='Job', compute='_compute_from_employee_id')
@@ -60,31 +75,24 @@ class LeaveEncashment(models.Model):
     address_id = fields.Many2one('res.partner', compute='_compute_from_employee_id', store=True, readonly=False, copy=True, string="Employee Home Address", check_company=True)
 
 
-    date = fields.Date(string='Date', default=fields.Date.today())
+    date = fields.Date(string='Date', default=fields.Date.today(), required=True, states=READONLY_STATES,)
     account_move_id = fields.Many2one('account.move', string='Journal Entry', ondelete='restrict', copy=False, readonly=True)
     accounting_date = fields.Date(
         string='Accounting Date',
         compute='_compute_accounting_date',
         store=True
     )
-    journal_id = fields.Many2one('account.journal', string='Accounting Journal', states={'done': [('readonly', True)], 'post': [('readonly', True)]}, check_company=True, domain="[('type', '=', 'purchase'), ('company_id', '=', company_id)]",
+    journal_id = fields.Many2one('account.journal', string='Accounting Journal', readonly=True, check_company=True, domain="[('type', '=', 'purchase'), ('company_id', '=', company_id)]",
         default=_default_journal_id, help="The journal used when the request is done.")
     company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True, states={'draft': [('readonly', False)]}, default=lambda self: self.env.company)
-    currency_id = fields.Many2one('res.currency', string='Currency', states={'draft': [('readonly', False)]},
-                                  compute='_compute_currency_id', store=True, readonly=True)
+    currency_id = fields.Many2one('res.currency', string='Currency',                                 compute='_compute_currency_id', store=True, readonly=True)
 
     # duration
     number_of_days = fields.Float(
-        'Number of Days', compute='_compute_from_holiday_status_id', store=True, readonly=False, tracking=True, default=1,
+        'Number of Days', store=True, readonly=False, tracking=True, default=1, required=True,
         help='Duration in days. Reference field to use when necessary.')
-    number_of_days_display = fields.Float(
-        'Duration (days)', compute='_compute_number_of_days_display',
-        states={'draft': [('readonly', False)], 'confirm': [('readonly', False)]},
-        help="If Accrual Allocation: Number of days allocated in addition to the ones you will get via the accrual' system.")
-    number_of_hours_display = fields.Float(
-        'Duration (hours)', compute='_compute_number_of_hours_display',
-        help="If Accrual Allocation: Number of hours allocated in addition to the ones you will get via the accrual' system.")
-    
+    leave_avail = fields.Float(string='Available Leaves', compute='_compute_all_balance')
+    leave_remain = fields.Float(string='Remaining Leaves', compute='_compute_all_balance')
     amount_total = fields.Float(string='Total Amount', compute='_compute_total', store=True)
     description = fields.Text(string='Description')
     
@@ -133,42 +141,94 @@ class LeaveEncashment(models.Model):
                     
     @api.depends('account_move_id.date')
     def _compute_accounting_date(self):
-        for sheet in self:
-            sheet.accounting_date = sheet.account_move_id.date
+        for record in self:
+            record.accounting_date = record.account_move_id.date
 
     @api.depends('holiday_status_id')
     def _compute_state(self):
         for leave in self:
             leave.state = 'draft' #'confirm' if leave.validation_type != 'no_validation' else 'draft'
 
-    @api.depends('holiday_status_id', 'allocation_type', 'number_of_hours_display', 'number_of_days_display', 'date_to')
-    def _compute_from_holiday_status_id(self):
-        accrual_allocations = self.filtered(lambda alloc: alloc.allocation_type == 'accrual' and not alloc.accrual_plan_id and alloc.holiday_status_id)
-        accruals_dict = {}
-        if accrual_allocations:
-            accruals_read_group = self.env['hr.leave.accrual.plan'].read_group(
-                [('time_off_type_id', 'in', accrual_allocations.holiday_status_id.ids)],
-                ['time_off_type_id', 'ids:array_agg(id)'],
-                ['time_off_type_id'],
-            )
-            accruals_dict = {res['time_off_type_id'][0]: res['ids'] for res in accruals_read_group}
-        for allocation in self:
-            allocation.number_of_days = allocation.number_of_days_display
-            if allocation.type_request_unit == 'hour':
-                allocation.number_of_days = allocation.number_of_hours_display / \
-                    (allocation.employee_id.sudo().resource_calendar_id.hours_per_day \
-                    or allocation.holiday_status_id.company_id.resource_calendar_id.hours_per_day \
-                    or HOURS_PER_DAY)
-            if allocation.accrual_plan_id.time_off_type_id.id not in (False, allocation.holiday_status_id.id):
-                allocation.accrual_plan_id = False
-            if allocation.allocation_type == 'accrual' and not allocation.accrual_plan_id:
-                if allocation.holiday_status_id:
-                    allocation.accrual_plan_id = accruals_dict.get(allocation.holiday_status_id.id, [False])[0]
+    def _compute_all_balance(self):
+        for record in self:
+            allocation_ids = self.env['hr.leave.allocation'].search([('employee_id','=',record.employee_id.id),('holiday_status_id','=',record.holiday_status_id.id),('state','=','validate')])
+            leave_ids = self.env['hr.leave'].search([('employee_id','=',record.employee_id.id),('holiday_status_id','=',record.holiday_status_id.id),('state','=','validate')])
+            record.leave_avail = sum(allocation_ids.mapped('number_of_days_display')) - sum(leave_ids.mapped('number_of_days_display'))
+            record.leave_remain = (sum(allocation_ids.mapped('number_of_days_display')) - sum(leave_ids.mapped('number_of_days_display'))) - record.number_of_days 
 
-    @api.depends('number_of_days')
-    def _compute_number_of_days_display(self):
-        for allocation in self:
-            allocation.number_of_days_display = allocation.number_of_days
+
+
+    @api.depends('state', 'employee_id', 'department_id')
+    def _compute_can_reset(self):
+        for holiday in self:
+            try:
+                holiday._check_approval_update('draft')
+            except (AccessError, UserError):
+                holiday.can_reset = False
+            else:
+                holiday.can_reset = True
+    @api.depends('state', 'employee_id', 'department_id')
+    def _compute_can_approve(self):
+        for holiday in self:
+            try:
+                if holiday.state == 'confirm' and holiday.validation_type == 'both':
+                    holiday._check_approval_update('validate1')
+                else:
+                    holiday._check_approval_update('validate')
+            except (AccessError, UserError):
+                holiday.can_approve = False
+            else:
+                holiday.can_approve = True
+    @api.depends_context('uid')
+    @api.depends('state', 'employee_id')
+    def _compute_can_cancel(self):
+        now = fields.Datetime.now()
+        for leave in self:
+            leave.can_cancel = leave.id and leave.employee_id.user_id == self.env.user and leave.state == 'validate' and leave.date_from and leave.date_from > now
+
+    def _check_approval_update(self, state):
+        """ Check if target state is achievable. """
+        if self.env.is_superuser():
+            return
+
+        current_employee = self.env.user.employee_id
+        is_officer = self.env.user.has_group('hr_holidays.group_hr_holidays_user')
+        is_manager = self.env.user.has_group('hr_holidays.group_hr_holidays_manager')
+
+        for holiday in self:
+            val_type = holiday.validation_type
+
+            if not is_manager and state != 'confirm':
+                if state == 'draft':
+                    if holiday.state == 'refuse':
+                        raise UserError(_('Only a Time Off Manager can reset a refused leave.'))
+                    if holiday.date_from and holiday.date_from.date() <= fields.Date.today():
+                        raise UserError(_('Only a Time Off Manager can reset a started leave.'))
+                    if holiday.employee_id != current_employee:
+                        raise UserError(_('Only a Time Off Manager can reset other people leaves.'))
+                else:
+                    if val_type == 'no_validation' and current_employee == holiday.employee_id:
+                        continue
+                    # use ir.rule based first access check: department, members, ... (see security.xml)
+                    holiday.check_access_rule('write')
+
+                    # This handles states validate1 validate and refuse
+                    if holiday.employee_id == current_employee:
+                        raise UserError(_('Only a Time Off Manager can approve/refuse its own requests.'))
+
+                    if (state == 'validate1' and val_type == 'both') and holiday.holiday_type == 'employee':
+                        if not is_officer and self.env.user != holiday.employee_id.leave_manager_id:
+                            raise UserError(_('You must be either %s\'s manager or Time off Manager to approve this leave') % (holiday.employee_id.name))
+
+                    if (state == 'validate' and val_type == 'manager') and self.env.user != (holiday.employee_id | holiday.sudo().employee_ids).leave_manager_id:
+                        if holiday.employee_id:
+                            employees = holiday.employee_id
+                        else:
+                            employees = ', '.join(holiday.employee_ids.filtered(lambda e: e.leave_manager_id != self.env.user).mapped('name'))
+                        raise UserError(_('You must be %s\'s Manager to approve this leave', employees))
+
+                    if not is_officer and (state == 'validate' and val_type == 'hr') and holiday.holiday_type == 'employee':
+                        raise UserError(_('You must either be a Time off Officer or Time off Manager to approve this leave'))     
             
     @api.model
     def create(self, vals):
@@ -180,35 +240,48 @@ class LeaveEncashment(models.Model):
 
     def _compute_journal_count(self):
         for record in self:
-            journal_total = self.env['journal.entry'].search_count([('employee_id', '=', record.employee_id.id)])
+            journal_total = self.env['account.move'].search_count([('id', '=', record.account_move_id.id)])
             record.journal_count = journal_total
 
+    @api.depends('number_of_days', 'contract_id')
     def _compute_total(self):
         for record in self:
-            record.total = record.leave * record.amount
+            if record.contract_id:
+                wage = record.contract_id.wage
+                if wage:
+                    days = int(record.number_of_days)  # Extract integer part (days)
+                    hours = (record.number_of_days - days) * 8  # Extract decimal part (hours)
+                    
+                    daily_wage = wage / 30  # Assuming 30 days in a month
+                    total_wage = (days + hours/8) * daily_wage  # Calculate total wage
+                    
+                    record.amount_total = total_wage
+                else:
+                    record.amount_total = 0.0
+            else:
+                record.amount_total = 0.0
+                
 
-    @api.depends('employee_id')
-    def _compute_remaining_leave(self):
-        for record in self:
-            leave_rec = self.env['hr.leave.allocation'].search(
-                [('employee_id', '=', record.employee_id.id), ('state', '=', 'validate')])
-            record.remaining_leave = 0 #leave_rec.number_of_days - record.leave
 
-    @api.depends('leave')
-    def _compute_encasenmet_leave(self):
-        for record in self:
-            record.encasenmet_leave = record.leave
 
     def action_draft(self):
         self.write({'state': 'draft'})
 
+    def action_confirm(self):
+        self.write({'state': 'draft'})
+
+    def action_validate(self):
+        self.write({'state': 'draft'})
+        
     def action_approve(self):
-        self.write({'state': 'approve'})
+        self.write({'state': 'confirm'})
+        return True
+        
 
     def action_paid(self):
         self.write({'state': 'paid'})
         for rec in self:
-            journal_rec = self.env['journal.entry'].search([])
+            journal_rec = self.env['account.move'].search([])
             values = {
                 'number': rec.bank_number,
                 'employee_id': rec.employee_id.id,
@@ -218,6 +291,9 @@ class LeaveEncashment(models.Model):
             }
             journal_rec.create(values)
 
+    def action_refuse(self):
+        self.write({'state': 'cancel'})
+        
     def action_cancel(self):
         self.write({'state': 'cancel'})
 
@@ -228,7 +304,7 @@ class LeaveEncashment(models.Model):
             'name': 'Journal Entry',
             'view_mode': 'tree,form',
             'target': 'current',
-            'res_model': 'journal.entry',
-            'domain': [('employee_id', '=', self.employee_id.id)],
+            'res_model': 'account.move',
+            'domain': [('id', '=', self.account_move_id.id)],
             'context': "{'create': False}"
         }
