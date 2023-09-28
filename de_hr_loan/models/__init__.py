e9e87b4c1c5f6021c3660a68fb64c44b7fa845b1
farhan <farhan@dynexcel.com>
2023-09-28 19:54:27 +0000

    update loan

--- de_hr_loan/models/__init__.py
+++ de_hr_loan/models/__init__.py
@@ -1,3 +1,4 @@
 from . import hr_loan
 from . import hr_loan_type
+from . import account_move
 
--- de_hr_loan/models/hr_loan.py
+++ de_hr_loan/models/hr_loan.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta, time
 from pytz import timezone, UTC
 from odoo.tools import date_utils
 from odoo.addons.base.models.res_partner import _tz_get
-
+from dateutil.relativedelta import relativedelta
 
 class HrLoan(models.Model):
     _name = 'hr.loan'
@@ -16,6 +16,12 @@ class HrLoan(models.Model):
     @api.model
     def _default_employee_id(self):
         return self.env.user.employee_id
+
+    @api.model
+    def _default_journal_id(self):
+        default_company_id = self.default_get(['company_id']).get('company_id')
+        journal = self.env['account.journal'].search([('type', '=', 'purchase'), ('company_id', '=', default_company_id)], limit=1)
+        return journal.id
 
     def _get_employee_id_domain(self):
         res = [('id', '=', 0)] # Nothing accepted by domain, by default
@@ -71,20 +77,34 @@ class HrLoan(models.Model):
     employee_id = fields.Many2one('hr.employee', string="Employee", required=True, readonly=False, tracking=True,
                                   states=READONLY_STATES, default=_default_employee_id, check_company=True, 
                                   domain=lambda self: self.env['hr.loan']._get_employee_id_domain())
-    department_id = fields.Many2one('hr.department', related="employee_id.department_id", readonly=True,
-                                    string="Department", help="Employee")
+    
+    loan_type_id = fields.Many2one("hr.loan.type", store=True, string="Loan Type", 
+                                   required=True, readonly=False, states=READONLY_STATES,tracking=True)
     company_id = fields.Many2one(related='employee_id.company_id', readonly=True, store=True)
     currency_id = fields.Many2one('res.currency', string='Currency',                               
                                   compute='_compute_currency_id', store=True, readonly=True)
+    loan_amount = fields.Float(string="Loan Amount", required=True, help="Loan amount",states=READONLY_STATES,)
+    installment = fields.Integer(string="No Of Installments", default=1, help="Number of installments", states=READONLY_STATES,)
+    date_start = fields.Date(string="Loan Start Date", required=True, default=fields.Date.today(), 
+                               help="Date of Start", states=READONLY_STATES,)
+    department_id = fields.Many2one('hr.department', string='Department', compute='_compute_from_employee_id', store=True)
+    job_id = fields.Many2one('hr.job', string='Job', compute='_compute_from_employee_id')
 
-    installment = fields.Integer(string="No Of Installments", default=1, help="Number of installments")
-    payment_date = fields.Date(string="Payment Start Date", required=True, default=fields.Date.today(), help="Date of "
-                                                                                                             "the "
-                                                                                                             "paymemt")
-    loan_lines = fields.One2many('hr.loan.line', 'loan_id', string="Loan Line", index=True)
-    job_position = fields.Many2one('hr.job', related="employee_id.job_id", readonly=True, string="Job Position",
-                                   help="Job position")
-    loan_amount = fields.Float(string="Loan Amount", required=True, help="Loan amount")
+    account_move_id = fields.Many2one('account.move', string='Journal Entry', ondelete='restrict', copy=False, readonly=True)
+    accounting_date = fields.Date(
+        string='Accounting Date',
+        compute='_compute_accounting_date',
+        states={'validate': [('readonly', False)], 'post': [('readonly', False)]},
+        store=True
+    )
+    journal_id = fields.Many2one('account.journal', string='Accounting Journal', check_company=True, domain="[('type', '=', 'purchase'), ('company_id', '=', company_id)]",
+        states={'validate': [('readonly', False)], 'post': [('readonly', False)]},
+        default=_default_journal_id, help="The journal used when the request is done.")
+    address_id = fields.Many2one('res.partner', compute='_compute_from_employee_id', store=True, readonly=False, copy=True, string="Employee Home Address", check_company=True)
+
+    loan_lines = fields.One2many('hr.loan.line', 'loan_id', string="Loan Line", index=True, states=READONLY_STATES,)
+    
+    
     total_amount = fields.Float(string="Total Amount", store=True, readonly=True, compute='_compute_loan_amount',
                                 help="Total loan amount")
     balance_amount = fields.Float(string="Balance Amount", store=True, compute='_compute_loan_amount',
@@ -94,6 +114,7 @@ class HrLoan(models.Model):
 
     state = fields.Selection([
         ('draft', 'To Submit'),
+        ('verify', 'Verified'),
         ('confirm', 'To Approve'),
         ('refuse', 'Refused'),
         ('validate1', 'Second Approval'),
@@ -141,7 +162,7 @@ class HrLoan(models.Model):
             """
         for loan in self:
             loan.loan_lines.unlink()
-            date_start = datetime.strptime(str(loan.payment_date), '%Y-%m-%d')
+            date_start = datetime.strptime(str(loan.date_start), '%Y-%m-%d')
             amount = loan.loan_amount / loan.installment
             for i in range(1, loan.installment + 1):
                 self.env['hr.loan.line'].create({
@@ -151,8 +172,36 @@ class HrLoan(models.Model):
                     'loan_id': loan.id})
                 date_start = date_start + relativedelta(months=1)
             loan._compute_loan_amount()
+            loan.write({
+                'state': 'verify',
+            })
         return True
 
+    @api.depends('account_move_id.date')
+    def _compute_accounting_date(self):
+        for record in self:
+            record.accounting_date = record.account_move_id.date
+
+    @api.depends('employee_id')
+    def _compute_from_employee_id(self):
+        for record in self:
+            record.address_id = record.employee_id.sudo().address_home_id.id
+            record.department_id = record.employee_id.sudo().department_id
+            record.job_id = record.employee_id.sudo().job_id
+
+    # ------------------------------------------------
+    # -------------- Action Buttons ------------------
+    # ------------------------------------------------
+    def action_draft(self):
+        for record in self:
+            record.write({
+                'state':'draft'
+            })
+    def action_confirm(self):
+        for record in self:
+            record.write({
+                'state':'confirm'
+            })
     def action_refuse(self):
         template = self.env.ref('de_hr_loan.loan_reject_email_temp')
         template.send_mail(self.id, force_send=True)
