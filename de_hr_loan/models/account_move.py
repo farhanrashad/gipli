da97dd18d922a6d9e98dfaee4b8b7926c9a986ea
Farhan Rashad <farhan@dynexcel.com>
2023-09-30 13:58:32 +0500

    Revert "Update loan"
    This reverts commit ad094d7aa582602bc4918bb18e43e203b75101fc.

--- de_hr_loan/models/account_move.py
+++ de_hr_loan/models/account_move.py
@@ -5,12 +5,9 @@ from collections import defaultdict
 
 from odoo import models, fields, api, _
 from odoo.tools.misc import frozendict
-from odoo.exceptions import AccessError, UserError, ValidationError
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     loan_id = fields.One2many('hr.loan', 'account_move_id')
-
-    
