cb6e20723ff7d6404b80921598c8e3854d5794c4
farhan <farhan@dynexcel.com>
2023-10-06 12:06:41 +0000

    update connector

--- de_apollo_connector/models/crm_lead.py
+++ de_apollo_connector/models/crm_lead.py
@@ -11,3 +11,7 @@ class CRMLead(models.Model):
     )
     ap_date_sync = fields.Date('Synronization Date', help="he date of the most recent synchronization of contacts with Apollo.")
 
+    update_required_for_apollo = fields.Boolean('Update Required for Apollo', help="Set to 'True' when this record requires an update in Apollo.")
+
+    
+
