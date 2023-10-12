aa96f84549b1bae1a529f0fe645b045ed2dd2b24
farhan <farhan@dynexcel.com>
2023-10-12 08:21:56 +0000

    update connector

--- de_apollo_connector/__manifest__.py
+++ de_apollo_connector/__manifest__.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Apollo Connector",
-
     'summary': """
-        Odoo Apollo Integration
+        Effortlessly Sync Opportunities and Contacts Between Odoo and Apollo
     """,
-
     'description': """
-        The Odoo Apollo Integration module seamlessly connects your Odoo CRM with Apollo, enabling streamlined prospecting and lead enrichment. Search and import leads from Apollo, enrich lead data, synchronize information in real-time, and automate tasks, all within your Odoo CRM. Elevate your lead management with accurate, up-to-date data and efficient workflows.
+        Our Apollo-Odoo Integration Module is your key to seamless lead and contact management. This module facilitates the easy import and export of opportunities and contacts between Odoo and Apollo, ensuring that your sales and marketing teams stay synchronized and efficient. Say goodbye to data duplication and hello to a streamlined workflow that maximizes your lead generation efforts. Experience a new level of productivity and collaboration by integrating these two powerful platforms.
     """,
     'author': "Dynexcel",
     'website': "https://www.dynexcel.com",
     'category': 'Sales/Marketing',
-    'version': '0.1',
-    # any module necessary for this one to work correctly
+    'version': '0.2',
+    'live_test_url': 'https://youtu.be/BwokAjL_KZU',
     'depends': ['crm'],
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'data/apollo_data.xml',
@@ -28,7 +25,7 @@
         'views/crm_tag_views.xml',
         'views/crm_stage_views.xml',
         'wizards/people_search_wizard_views.xml',
-        'wizards/companies_search_wizard_views.xml',
+        #'wizards/companies_search_wizard_views.xml',
         'wizards/apl_ops_wizard_views.xml',
         'views/apl_people_views.xml',
         'views/apl_companies_views.xml',
@@ -41,8 +38,11 @@
            'de_apollo_connector/static/src/xml/button_search.xml',
        ],
     },
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'license': 'OPL-1',
+    'price': 110,
+    'currency': 'USD',
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
