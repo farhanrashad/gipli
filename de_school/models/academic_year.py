b529f1f0aa39f3178d2879840b5378f5f910568c
farhan <farhan@dynexcel.com>
2023-10-14 17:05:49 +0000

    update school

--- de_school/models/academic_year.py
+++ de_school/models/academic_year.py
@@ -9,4 +9,16 @@ class SchoolAcademicYear(models.Model):
     name = fields.Char('Name',required=True)
     date_start = fields.Date('Date Start', required = True)
     date_end = fields.Date('Date end',required = True)
-    active = fields.Boolean('Active')
+    active = fields.Boolean('Active', default=True)
+
+    @api.constrains('date_start', 'date_end')
+    def _check_date_overlap(self):
+        for year in self:
+            # Check for overlapping dates in other academic years
+            overlapping_years = self.search([
+                ('id', '!=', year.id),
+                ('date_start', '<=', year.date_end),
+                ('date_end', '>=', year.date_start),
+            ])
+            if overlapping_years:
+                raise ValidationError("Academic years cannot overlap with each other.")
--- de_school/models/batch.py
+++ de_school/models/batch.py
@@ -7,15 +7,31 @@ from odoo.exceptions import UserError, AccessError
 class SchoolCourseBatch(models.Model):
     _name = 'oe.school.course.batch'
     _description = 'Course Batch'
-    
+
+    def _default_year_id(self):
+        # Search for the active academic year in oe.school.year
+        active_year = self.env['oe.school.year'].search([('active', '=', True)], limit=1)
+        return active_year.id if active_year else False
+        
     name = fields.Char(string='Batch', required=True, index=True, translate=True)
     course_id = fields.Many2one('oe.school.course', string='Course', required=True)
     subject_ids = fields.Many2many('oe.school.course.subject', compute='_compute_subjects')
     company_id = fields.Many2one('res.company', string='Company', index=True, default=lambda self: self.env.company)
-    date_start = fields.Date(string='Start Date', required=True)
-    date_end = fields.Date(string='End Date', required=True)
+    year_id = fields.Many2one('oe.school.year', string='Year', required=True, 
+                           default=lambda self: self._default_year_id(),
+                           domain="[('active','=',True)]")
+    date_start = fields.Date(string='Start Date', required=True, readonly=False,
+                             store=True, compute='_compute_date_from_school_year')
+    date_end = fields.Date(string='End Date', required=True, readonly=False,
+                           store=True, compute='_compute_date_from_school_year')
     
     def _compute_subjects(self):
         subject_ids = self.env['oe.school.course.subject'].search([('batch_ids','in', self.id)])
         self.subject_ids = subject_ids.ids
+
+    @api.depends('year_id')
+    def _compute_date_from_school_year(self):
+        for record in self:
+            record.date_start = record.year_id.date_start
+            record.date_end = record.year_id.date_end
     
