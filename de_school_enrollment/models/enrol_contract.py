76d78cb4262c8d064ed9f705485980148f087fda
farhan <farhan@dynexcel.com>
2023-10-24 19:37:23 +0000

    Update enrollment

--- de_school_enrollment/models/enrol_contract.py
+++ de_school_enrollment/models/enrol_contract.py
@@ -63,7 +63,7 @@ class EnrollmentContract(models.Model):
     def _compute_admission_team_id(self):
         cached_teams = {}
         for order in self:
-            default_team_id = self.env.context.get('default_admission_team_id', False) or order.team_id.id or order.partner_id.team_id.id
+            default_team_id = self.env.context.get('default_admission_team_id', False) or order.admission_team_id.id or order.partner_id.admission_team_id.id
             user_id = order.user_id.id
             company_id = order.company_id.id
             key = (default_team_id, user_id, company_id)
