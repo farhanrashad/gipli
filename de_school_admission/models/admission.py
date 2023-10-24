8a3d01f417a5f90c9649e5c39c50b31f6d3de775
farhan <farhan@dynexcel.com>
2023-10-24 17:06:05 +0000

    Update admission

--- de_school_admission/models/admission.py
+++ de_school_admission/models/admission.py
@@ -856,7 +856,7 @@ class Admission(models.Model):
             'name': partner_name,
             'user_id': self.env.context.get('default_user_id') or self.user_id.id,
             'comment': self.description,
-            'team_id': self.team_id.id,
+            #'team_id': self.team_id.id,
             'parent_id': parent_id,
             'phone': self.phone,
             'mobile': self.mobile,
