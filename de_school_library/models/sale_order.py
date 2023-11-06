f829724f58b2e3c7aa9a233e5b13ed60cc7d8f7e
farhan <farhan@dynexcel.com>
2023-11-06 18:44:46 +0000

    update librarry

--- de_school_library/models/sale_order.py
+++ de_school_library/models/sale_order.py
@@ -23,38 +23,40 @@ class CirculationAgreement(models.Model):
     next_action_date = fields.Datetime(
         string="Next Action", compute='_compute_next_action_date', store=True)
 
+    #has_pickable_lines = fields.Boolean(compute="_compute_rental_status", store=True)
     #has_late_lines = fields.Boolean(compute="_compute_has_late_lines")
 
     # compute Method
     #@api.depends('state', 'order_line', 'order_line.product_uom_qty', 'order_line.qty_delivered', 'order_line.qty_returned')
     def _compute_next_action_date(self):
         for order in self:
-            if order.state in ['sale', 'done'] and order.is_rental_order:
+            if order.state in ['sale', 'done'] and order.is_borrow_order:
                 rental_order_lines = order.order_line.filtered(lambda l: l.is_rental and l.start_date and l.return_date)
                 pickeable_lines = rental_order_lines.filtered(lambda sol: sol.qty_delivered < sol.product_uom_qty)
                 returnable_lines = rental_order_lines.filtered(lambda sol: sol.qty_returned < sol.qty_delivered)
                 min_pickup_date = min(pickeable_lines.mapped('start_date')) if pickeable_lines else 0
                 min_return_date = min(returnable_lines.mapped('return_date')) if returnable_lines else 0
                 if min_pickup_date and pickeable_lines and (not returnable_lines or min_pickup_date <= min_return_date):
-                    order.rental_status = 'pickup'
+                    order.borrow_status = 'pickup'
                     order.next_action_date = min_pickup_date
                 elif returnable_lines:
-                    order.rental_status = 'return'
+                    order.borrow_status = 'return'
                     order.next_action_date = min_return_date
                 else:
-                    order.rental_status = 'returned'
+                    order.borrow_status = 'return'
                     order.next_action_date = False
-                order.has_pickable_lines = bool(pickeable_lines)
-                order.has_returnable_lines = bool(returnable_lines)
+                #order.has_pickable_lines = bool(pickeable_lines)
+                #order.has_returnable_lines = bool(returnable_lines)
             else:
-                order.has_pickable_lines = False
-                order.has_returnable_lines = False
-                order.rental_status = order.state if order.is_rental_order else False
+                #order.has_pickable_lines = False
+                #order.has_returnable_lines = False
+                #order.rental_status = order.state if order.is_borrow_order else False
                 order.next_action_date = False
-    @api.depends('is_rental_order', 'next_action_date', 'rental_status')
+    
+    @api.depends('is_borrow_order', 'next_action_date', 'rental_status')
     def _compute_has_late_lines(self):
         for order in self:
             order.has_late_lines = (
-                order.is_rental_order
+                order.is_borrow_order
                 and order.rental_status in ['pickup', 'return']  # has_pickable_lines or has_returnable_lines
                 and order.next_action_date and order.next_action_date < fields.Datetime.now())
