8d96ce7d0e778b571b7fcaa03bf0c3288f654870
Odoo Paas <paas-admin@odoo.com.test>
2024-01-22 21:29:01 +0000

    udpate hr service

---
+++ de_portal_hr_service/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Employee Self Service",
+
+    'summary': "Employee Self Service",
+
+    'description': """
+Employee Self Service
+    """,
+
+    'author': "Dynexcel",
+    'website': "https://www.dynexcel.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
+
