ac19558d1a26ac264b16142a5329eb90de7ced19
Odoo Paas <paas-admin@odoo.com.test>
2024-01-30 23:42:33 +0000

    udpate chat gpt

---
+++ de_chatgpt/models/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from openai import OpenAI
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class Channel(models.Model):
+    _inherit = 'mail.channel'
