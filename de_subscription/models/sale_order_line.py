0f0eacaad729afa43b1c6788c7faff9f3371ca19
farhan <farhan@dynexcel.com>
2024-02-23 20:05:42 +0000

    update subscription

--- de_subscription/models/sale_order_line.py
+++ de_subscription/models/sale_order_line.py
@@ -22,7 +22,7 @@ class SubscriptionOrderLine(models.Model):
 
     is_recurring = fields.Boolean(related="product_template_id.is_recurring")
     amount_monthly_subscription = fields.Monetary(compute='_compute_monthly_subscription', string="Monthly Subscription")
-    parent_subscription_line_id = fields.Many2one('sale.order.line', compute='_compute_parent_line_id', store=True, precompute=True)
+    parent_subscription_line_id = fields.Many2one('sale.order.line', compute='_compute_parent_line', store=True, precompute=True)
 
     @api.depends('is_recurring', 'price_subtotal')
     def _compute_monthly_subscription(self):
@@ -32,18 +32,11 @@ class SubscriptionOrderLine(models.Model):
             else:
                 line.amount_monthly_subscription = line.price_subtotal * INTERVAL_FACTOR[line.order_id.subscription_plan_id.recurring_interval_type] / line.order_id.subscription_plan_id.recurring_interval
                 
-    def _compute_parent_line_id(self):
-        """
-        Compute the link between a SOL and the line in the parent order. The matching is done based on several
-        fields values like the price_unit, the uom, etc. The method does not depend on pricelist_id or currency_id
-        on purpose because '_compute_price_unit' depends on 'parent_line_id' and it triggered side effects
-        when we added these dependencies.
-        """
+    def _compute_parent_line(self):
         parent_line_ids = self.order_id.parent_subscription_id.order_line
         for line in self:
-            if not line.order_id.parent_subscription_id: #or not line.product_id.recurring_invoice:
+            if not line.product_id.is_recurring:
                 continue
-            # We use a rounding to avoid -326.40000000000003 != -326.4 for new records.
             matching_line_ids = parent_line_ids.filtered(
                 lambda l:
                 (l.order_id, l.product_id, l.product_uom, l.order_id.currency_id, l.order_id.subscription_plan_id,
@@ -96,55 +89,3 @@ class SubscriptionOrderLine(models.Model):
             }))
     
         return order_lines
-
-    
-    def _get_renew_order_values1(self, subscription_id, period_end=None):
-        order_lines = []
-        description_needed = False #self._need_renew_discount_info()
-        today = fields.Date.today()
-        for line in self:
-            #if not line.recurring_invoice:
-            #    continue
-            partner_lang = line.order_id.partner_id.lang
-            line = line.with_context(lang=partner_lang) if partner_lang else line
-            product = line.product_id
-            order_lines.append((0, 0, {
-                'parent_subscription_line_id': line.id,
-                'name': line.name,
-                'product_id': product.id,
-                'product_uom': line.product_uom.id,
-                'product_uom_qty': 0 if subscription_id.subscription_type == 'upsell' else line.product_uom_qty,
-                'price_unit': line.price_unit,
-            }))
-            description_needed = True
-
-        if subscription_id.subscription_type == 'upsell' and description_needed and period_end:
-            start_date = max(today, line.order_id.first_contract_date or today)
-            end_date = period_end - relativedelta(days=1)  # the period ends the day before the next invoice
-            if start_date >= end_date:
-                line_name = _('Recurring products are entirely discounted as the next period has not been invoiced yet.')
-            else:
-                format_start = format_date(self.env, start_date)
-                format_end = format_date(self.env, end_date)
-                line_name = _('Recurring products are discounted according to the prorated period from %s to %s', format_start, format_end)
-
-            order_lines.append((0, 0,
-                {
-                    'display_type': 'line_note',
-                    'sequence': 999,
-                    'name': line_name,
-                    'product_uom_qty': 0
-                }
-            ))
-
-        return order_lines
-
-    def _reset_subscription_qty_to_invoice(self):
-        """ Define the qty to invoice on subscription lines equal to product_uom_qty for recurring lines
-            It allows avoiding using the _compute_qty_to_invoice with a context_today
-        """
-        today = fields.Date.today()
-        for line in self:
-            if not line.is_recurring or line.product_id.invoice_policy == 'delivery' or line.order_id.date_start and line.order_id.date_start > today:
-                continue
-            line.qty_to_invoice = line.product_uom_qty
