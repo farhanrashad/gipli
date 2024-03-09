b0b8c0c1492d3add1599871d86a09009ffe41f68
farhan <farhan@dynexcel.com>
2024-03-09 10:08:56 +0000

    update gym

--- de_gym/__manifest__.py
+++ de_gym/__manifest__.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 {
     'name': "GYM Management",
-
     'summary': "GYM Management",
-
     'description': """
-GYM Manager
-    """,
+Membership - GYM Management App
+================================
 
+Features:
+
+    - Manage member registrations, memberships, and payments effortlessly
+    - Create and customize workout plans tailored to individual needs
+    - Schedule and organize fitness classes with ease
+    - Track attendance and member progress to optimize workouts
+    - Monitor equipment maintenance and ensure gym safety
+    - Offer nutrition plans and guidance for a holistic approach to fitness
+    - Generate reports on membership trends, revenue, and class popularity
+    - Integrate with payment systems and fitness tracking apps for seamless operations.
+    
+""",
     'author': "Dynexcel",
     'website': "https://www.dynexcel.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Sale',
+    'support': "info@dynexcel.com",
+    'category': 'Sales/Subscriptions',
     'version': '17.0.0.1',
-
-    # any module necessary for this one to work correctly
+    'live_test_url': 'https://youtu.be/_qKgJmMrVq4',
     'depends': ['de_subscription','hr'],
-
-    # always loaded
     'data': [
         'security/gym_security.xml',
         'security/ir.model.access.csv',
@@ -48,12 +52,12 @@ GYM Manager
         'views/nutr_planning_line_views.xml',
         'wizards/nutr_plan_wizard_views.xml',
     ],
-    #'assets': {
-    #    'web.assets_backend': [
-    #        'de_gym/static/src/js/nutr_schedule_button_calendar.js',
-    #        'de_gym/static/src/xml/nutr_schedule_button.xml',
-    #    ],
-    #},
-    'application':True,
+    'license': 'OPL-1',
+    'price': 30,
+    'currency': 'USD',
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
 
