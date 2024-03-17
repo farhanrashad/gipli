15e75a87f05f4cdcd0e296d4dc0419477a952c25
farhan <farhan@dynexcel.com>
2024-03-17 08:03:28 +0000

    add payment

---
+++ de_payment_automatic/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class de_payment_autorun(models.Model):
+#     _name = 'de_payment_autorun.de_payment_autorun'
+#     _description = 'de_payment_autorun.de_payment_autorun'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
