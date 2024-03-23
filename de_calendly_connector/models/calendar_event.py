8f3dbf2034931f562590103b1a48d7b1d789011c
farhan <farhan@dyenxcel.com>
2024-03-23 17:59:15 +0000

    update calendly

--- de_calendly_connector/models/calendar_event.py
+++ de_calendly_connector/models/calendar_event.py
@@ -12,10 +12,29 @@ class CalendarEvent(models.Model):
 
     def test_users(self):
         company_id = self.env.user.company_id
+        user_data = company_id.get_current_user()
+        name = user_data['resource']['name']
+        raise UserError(name)
+        
+        raise UserError(company_id.get_current_user())
 
         access_token = company_id.calendly_access_token
         refresh_token = company_id.calendly_refresh_token
 
+        # Schedule Events List
+        if access_token:
+            # Get list of event types
+            response = requests.get(
+                'https://api.calendly.com/scheduled_events', 
+                headers={
+                    'Authorization': f'Bearer {access_token}',
+                    'Accept': 'application/json'
+                }
+            )
+            #user_data = response.json()
+            #user_data_str = json.dumps(user_data, indent=4)
+            #raise UserError(user_data_str)
+            
         #raise UserError(access_token)
         
         if access_token:
@@ -30,16 +49,9 @@ class CalendarEvent(models.Model):
 
             user_data = event_types_response.json()
             name = user_data['resource']['name']
-            raise UserError(name)
-
-            
-            # Parse the JSON response
-            parsed_response = json.loads(event_types_response)
-            
-            # Extract the name field
-            uname = parsed_response['resource']['name']
+            #raise UserError(name)
 
 
             user_data = event_types_response.json()
             user_data_str = json.dumps(user_data, indent=4)
-            raise UserError(uname)
+            raise UserError(user_data_str)
--- de_calendly_connector/models/company.py
+++ de_calendly_connector/models/company.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
+import requests
+from odoo import models, fields
+from odoo.exceptions import UserError
 
+CALENDLY_BASE_URL = 'https://api.calendly.com'
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
@@ -11,3 +14,42 @@ class ResCompany(models.Model):
     calendly_access_token = fields.Char(string='Access Token')
     calendly_refresh_token = fields.Char(string='Refresh Token')
     calendly_generated_access_token = fields.Boolean(string='Access Token Generated')
+
+    def _get_header(self):
+        access_token = self.calendly_access_token
+        if access_token:
+            return {
+                'Authorization': f'Bearer {access_token}', 
+                'Accept': 'application/json'
+            }
+        else:
+            raise UserError('Access token is not available.')
+        
+    def _handle_response(self, response):
+        if response.status_code == 401:
+            # Handle token refresh here if needed
+            raise UserError('Unauthorized. Token refresh logic goes here.')
+        elif not response.ok:
+            raise UserError(f'Error: {response.status_code} - {response.text}')
+        else:
+            return response.json()
+
+    def get_scheduled_events(self, organization=None, user=None):
+        url = f'{CALENDLY_BASE_URL}/scheduled_events'
+        params = {}
+        if organization:
+            params['organization'] = organization
+        if user:
+            params['user'] = user
+
+        headers = self._get_header()
+        response = requests.get(url, params=params, headers=headers)
+        return self._handle_response(response)
+
+    def get_current_user(self):
+        url = f'{CALENDLY_BASE_URL}/users/me'
+        params = {}
+        headers = self._get_header()
+        response = requests.get(url, params=params, headers=headers)
+        return self._handle_response(response)
+        
