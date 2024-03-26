657c22c25b253d60a8188873ceb96dd237d0e86b
farhan <farhan@dyenxcel.com>
2024-03-26 09:24:32 +0000

    update discord connector

--- de_discord_connector/__manifest__.py
+++ de_discord_connector/__manifest__.py
@@ -15,11 +15,13 @@ Discord Connector
     'version': '17.0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['mail','base_setup'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'data/res_partner_data.xml',
+        'data/res_users_data.xml',
         'views/res_config_setting_views.xml',
     ],
     # only loaded in demonstration mode
