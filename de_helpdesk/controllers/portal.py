bbd4cbc64bddf6077cd773d973cecc51f20555be
farhan <farhan@dynexcel.com>
2024-03-27 22:33:45 +0000

    update

--- de_helpdesk/controllers/portal.py
+++ de_helpdesk/controllers/portal.py
@@ -6,13 +6,17 @@ from odoo import conf, http, _
 from odoo.exceptions import AccessError, MissingError, UserError
 from odoo.http import request
 from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
-#from odoo.tools import groupby as groupbyelem
+from odoo.tools import groupby as groupbyelem
 
 from odoo.osv.expression import OR, AND
 
 
 class TicketCustomerPortal(CustomerPortal):
 
+    def _prepare_portal_layout_values(self):
+        values = super(TicketCustomerPortal, self)._prepare_portal_layout_values()
+        return values
+        
     def _prepare_home_portal_values(self, counters):
         values = super()._prepare_home_portal_values(counters)
         if 'tickets_count' in counters:
@@ -20,24 +24,18 @@ class TicketCustomerPortal(CustomerPortal):
                 if request.env['project.task'].check_access_rights('read', raise_exception=False) else 0
         return values
         
-    def _ticket_get_page_view_values(self, ticket_id, access_token, **kwargs):
-        
+    def _get_ticket_page_view_values(self, ticket_id, access_token, **kwargs):
         page_name = 'ticket'
         history = 'my_tickets_history'
-        #try:
-        #    project_accessible = bool(ticket.project_id.id and self._document_check_access('project.project', ticket.project_id.id))
-        #except (AccessError, MissingError):
-        #    project_accessible = False
-        ticket = request.env['project.task'].browse(ticket_id)
-        #raise UserError(ticket)
         values = {
             'page_name': page_name,
-            'ticket': ticket.sudo(),
+            'ticket': ticket_id,
             'user': request.env.user,
-            #'project_accessible': project_accessible,
             'ticket_link_section': [],
-            'preview_object': ticket,
+            'preview_object': ticket_id,
+            'object': ticket_id,
         }
+        return self._get_page_view_values(ticket_id, access_token, values, 'my_tickets_history', False, **kwargs)
 
     def _ticket_get_searchbar_sortings(self, project=False):
         values = {
@@ -79,7 +77,7 @@ class TicketCustomerPortal(CustomerPortal):
             return order
         return '%s, %s' % (field_name, order)
 
-    def _ticket_get_searchbar_inputs(self, project=False):
+    def _ticket_get_searchbar_inputs(self):
         values = {
             'all': {'input': 'all', 'label': _('Search in All'), 'order': 1},
             'content': {'input': 'content', 'label': Markup(_('Search <span class="nolabel"> (in Content)</span>')), 'order': 1},
@@ -120,13 +118,16 @@ class TicketCustomerPortal(CustomerPortal):
             search_domain.append([('state', 'ilike', state_dict.get(search, search))])
         return OR(search_domain)
 
+    def _prepare_tickets_domain(self):
+        return []
+        
     def _prepare_tickets_values(self, page, date_begin, date_end, sortby, search, search_in, groupby, url="/my/desk/tickets", domain=None, su=False, project=False):
         values = self._prepare_portal_layout_values()
 
         Ticket = request.env['project.task'].search([])
         searchbar_sortings = dict(sorted(self._ticket_get_searchbar_sortings(project).items(),
                                          key=lambda item: item[1]["sequence"]))
-        searchbar_inputs = self._ticket_get_searchbar_inputs(project)
+        searchbar_inputs = self._ticket_get_searchbar_inputs()
         #searchbar_groupby = self._ticket_get_searchbar_groupby(project)
 
         if not domain:
@@ -158,7 +159,7 @@ class TicketCustomerPortal(CustomerPortal):
             'date': date_begin,
             'date_end': date_end,
             #'grouped_tickets': get_grouped_tickets,
-            'page_name': 'tickets',
+            'page_name': 'ticket',
             'default_url': url,
             'ticket_url': 'tickets',
             'tickets': Ticket_sudo,
@@ -190,60 +191,127 @@ class TicketCustomerPortal(CustomerPortal):
             })
 
         return searchbar_filters
-    
-    def _get_my_tickets_searchbar_filters1(self, project_domain=None, ticket_domain=None):
+
+    def _prepare_tickets_values(self, page=1, date_begin=None, date_end=None, sortby=None, filterby='all', search=None, groupby='none', search_in='content'):
+        values = self._prepare_portal_layout_values()
+        domain = self._prepare_tickets_domain()
+
+        searchbar_sortings = {
+            'date': {'label': _('Newest'), 'order': 'create_date desc'},
+            #'reference': {'label': _('Reference'), 'order': 'id desc'},
+            'name': {'label': _('Subject'), 'order': 'name'},
+            #'user': {'label': _('Assigned to'), 'order': 'user_id'},
+            'stage': {'label': _('Stage'), 'order': 'stage_id'},
+            #'update': {'label': _('Last Stage Update'), 'order': 'date_last_stage_update desc'},
+        }
         searchbar_filters = {
-            'all': {'label': _('All'), 'domain': [('project_id', '!=', False)]},
+            #'all': {'label': _('All'), 'domain': []},
+            'assigned': {'label': _('Assigned'), 'domain': [('user_ids', '!=', False)]},
+            'unassigned': {'label': _('Unassigned'), 'domain': [('user_ids', '=', False)]},
+            #'open': {'label': _('Open'), 'domain': [('close_date', '=', False)]},
+            #'closed': {'label': _('Closed'), 'domain': [('close_date', '!=', False)]},
+        }
+        searchbar_inputs = {
+            'content': {'input': 'content', 'label': Markup(_('Search <span class="nolabel"> (in Content)</span>'))},
+            #'ticket_ref': {'input': 'ticket_ref', 'label': _('Search in Reference')},
+            'message': {'input': 'message', 'label': _('Search in Messages')},
+            'user': {'input': 'user', 'label': _('Search in Assigned to')},
+            'status': {'input': 'status', 'label': _('Search in Stage')},
+        }
+        searchbar_groupby = {
+            'none': {'input': 'none', 'label': _('None')},
+            'stage': {'input': 'stage_id', 'label': _('Stage')},
+            'user': {'input': 'user_ids', 'label': _('Assigned to')},
         }
 
-        # extends filterby criteria with project the customer has access to
-        projects = request.env['project.project'].search(project_domain or [])
-        for project in projects:
-            searchbar_filters.update({
-                str(project.id): {'label': project.name, 'domain': [('project_id', '=', project.id)]}
-            })
+        if not groupby:
+            groupby = 'none'
+        
+        # default sort by value
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
+        if groupby in searchbar_groupby and groupby != 'none':
+            order = f'{searchbar_groupby[groupby]["input"]}, {order}'
 
-        # extends filterby criteria with project (criteria name is the project id)
-        # Note: portal users can't view projects they don't follow
-        #project_groups = request.env['project.task']._read_group(AND([[('project_id', 'not in', projects.ids)], ticket_domain or []]),
-        #                                                        ['project_id'])
-        #for [project] in project_groups:
-        #    proj_name = project.sudo().display_name if project else _('Others')
-        #    searchbar_filters.update({
-        #        str(project.id): {'label': proj_name, 'domain': [('project_id', '=', project.id)]}
-        #    })
-        return searchbar_filters
+        
+        #else:
+        #    domain = AND([domain, searchbar_filters[filterby]['domain']])
 
+        if date_begin and date_end:
+            domain = AND([domain, [('create_date', '>', date_begin), ('create_date', '<=', date_end)]])
+
+        # search
+        if search and search_in:
+            search_domain = []
+            if search_in == 'ticket_ref':
+                search_domain = OR([search_domain, [('ticket_ref', 'ilike', search)]])
+            if search_in == 'content':
+                search_domain = OR([search_domain, ['|', ('name', 'ilike', search), ('description', 'ilike', search)]])
+            if search_in == 'user':
+                assignees = request.env['res.users'].sudo()._search([('name', 'ilike', search)])
+                search_domain = OR([search_domain, [('user_id', 'in', assignees)]])
+            if search_in == 'message':
+                discussion_subtype_id = request.env.ref('mail.mt_comment').id
+                search_domain = OR([search_domain, [('message_ids.body', 'ilike', search), ('message_ids.subtype_id', '=', discussion_subtype_id)]])
+            if search_in == 'status':
+                search_domain = OR([search_domain, [('stage_id', 'ilike', search)]])
+            domain = AND([domain, search_domain])
+
+        # pager
+        tickets_count = request.env['project.task'].search_count(domain)
+        pager = portal_pager(
+            url="/my/tickets",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby, 'search_in': search_in, 'search': search, 'groupby': groupby, 'filterby': filterby},
+            total=tickets_count,
+            page=page,
+            step=self._items_per_page
+        )
+
+        tickets = request.env['project.task'].search(domain, order=order, limit=self._items_per_page, offset=pager['offset'])
+        request.session['my_tickets_history'] = tickets.ids[:100]
+
+        if not tickets:
+            grouped_tickets = []
+        elif groupby != 'none':
+            grouped_tickets = [request.env['project.task'].concat(*g) for k, g in groupbyelem(tickets, itemgetter(searchbar_groupby[groupby]['input']))]
+        else:
+            grouped_tickets = [tickets]
+
+        tickets = request.env['project.task'].search([])
+        #raise UserError(tickets)
+        values.update({
+            'date': date_begin,
+            'grouped_tickets': grouped_tickets,
+            'tickets': tickets,
+            'page_name': 'ticket',
+            'default_url': '/my/desk/tickets',
+            'pager': pager,
+            'searchbar_sortings': searchbar_sortings,
+            #'searchbar_filters': searchbar_filters,
+            'searchbar_inputs': searchbar_inputs,
+            'searchbar_groupby': searchbar_groupby,
+            'sortby': sortby,
+            'groupby': groupby,
+            'search_in': search_in,
+            'search': search,
+            'filterby': filterby,
+        })
+        return values
     
     @http.route([
         '/my/desk/tickets', 
         '/my/desk/tickets/page/<int:page>'
     ], type='http', auth="user", website=True)
     def portal_my_tickets(self, page=1, date_begin=None, date_end=None, sortby='date', filterby=None, search=None, search_in='content', groupby=None, **kw):
-        searchbar_filters = self._get_my_tickets_searchbar_filters()
-
-        if not filterby:
-            filterby = 'all'
-        domain = searchbar_filters.get(filterby, searchbar_filters.get('all'))['domain']
-
-        values = self._prepare_tickets_values(page, date_begin, date_end, sortby, search, search_in, groupby, domain=domain)
-
-        # pager
-        pager_vals = values['pager']
-        pager_vals['url_args'].update(filterby=filterby)
-        pager = portal_pager(**pager_vals)
-
-        values.update({
-            #'grouped_tickets': values['grouped_tickets'](pager['offset']),
-            'pager': pager,
-            'searchbar_filters': OrderedDict(sorted(searchbar_filters.items())),
-            'filterby': filterby,
-        })
+        values = self._prepare_tickets_values(page, date_begin, date_end, sortby, filterby, search, groupby, search_in)
         return request.render("de_helpdesk.portal_my_tickets", values)
 
     @http.route([
         "/my/desk/ticket/<int:ticket_id>",
         "/my/desk/ticket/<int:ticket_id>/<access_token>",
+        '/my/desk/ticket/<int:ticket_id>',
+        '/my/desk/ticket/<int:ticket_id>/<access_token>'
     ], type='http', auth="public", website=True)
     def portal_my_ticket(self, ticket_id, report_type=None, access_token=None, project_sharing=False, **kw):
         try:
@@ -259,9 +327,9 @@ class TicketCustomerPortal(CustomerPortal):
             attachment.generate_access_token()
         
         # Add 'ticket' to the values dictionary
-        values = self._ticket_get_page_view_values(ticket_sudo, access_token, **kw)
+        values = self._get_ticket_page_view_values(ticket_sudo, access_token, **kw)
         
-        request.session['my_tickets_history'] = ticket_sudo.ids
+        #request.session['my_tickets_history'] = ticket_sudo.ids
         return request.render("de_helpdesk.portal_my_ticket", values)
 
     
