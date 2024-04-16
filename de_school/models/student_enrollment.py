3cdfb24e7c0c96c460a8aacbd48eb1c2583db6db
farhan <farhan@dynexcel.com>
2024-04-16 16:05:28 +0000

    udpate school

--- de_school/models/student_enrollment.py
+++ de_school/models/student_enrollment.py
@@ -17,9 +17,19 @@ class StudentEnrollment(models.Model):
 
     model = fields.Char('Related Document Model')
     res_id = fields.Many2oneReference('Related Document ID', model_field='model')
+    
     school_name = fields.Char('School Name', required=True)
-    school_year = fields.Char('School Year', required=True)
-    level_grade = fields.Char('Grading Level', required=True)
+    course_name = fields.Char('Program/Course', required=True)
     date_start = fields.Date('Start Date', required=True)
     date_end = fields.Date('End Date', required=True)
-    address_school = fields.Char('School Address')
+    status = fields.Selection([
+        ('enroll', 'Enrolled'),
+        ('complete', 'Completed'),
+        ('transfer', 'Transferred'),
+        ('withdrawn', 'Withdrawn'),
+        ('suspended', 'Suspended'),
+        ('other', 'Other'),
+    ], string='Status')
+    transcript_detail = fields.Text('Transcript')
+    reason = fields.Text(string='Reason for Leaving')    
+    address_school = fields.Text('School Address')
