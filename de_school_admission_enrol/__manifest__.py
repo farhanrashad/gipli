0b653b423e2bf6f6c43e08caf2f7f27c5a7c901b
farhan <farhan@dynexcel.com>
2024-04-17 20:10:22 +0000

    udpate enrollment

--- de_school_admission_enrol/__manifest__.py
+++ de_school_admission_enrol/__manifest__.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Admission to Enrollment",
+    'name': "Openrol - Admission to Enrollment",
     'summary': """
-    Generate Enrollment Contracts from Application
+    Integrate admission application with enrollment order
         """,
     'description': """
+School Managmeent - Enrollment App
+================================
+Features:
+
         The process of generating enrollment contracts from application submissions streamlines the admissions workflow, allowing educational institutions to efficiently convert student applications into formal enrollment agreements. This automated procedure simplifies administrative tasks and enhances the overall enrollment experience for both students and institutions by reducing manual paperwork and expediting the acceptance process.
     """,
     'author': "Dynexcel",
     'website': "https://www.dynexcel.com",
-    'category': 'Sales/School',
+    'category': 'Sales/School/Industries',
     'version': '0.1',
     'depends': ['de_school_admission','de_school_enrollment'],
     'data': [
@@ -21,4 +25,8 @@
         'views/admission_register_views.xml',
         'reports/report_enrol_order_views.xml',
     ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
