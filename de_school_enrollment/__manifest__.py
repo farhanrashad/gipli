20886979562945c917a72469127e3684b88affde
farhan <farhan@dynexcel.com>
2024-04-17 20:02:41 +0000

    update enrollment

--- de_school_enrollment/__manifest__.py
+++ de_school_enrollment/__manifest__.py
@@ -3,13 +3,22 @@
 # This module has been developed by Dynexcel to enhance the functionality and user experience of the system. Dynexcel, with its commitment to excellence, ensures that this module adheres to the highest standards of quality and performance. We appreciate feedback and suggestions to continually improve our offerings. For any queries or support, please reach out to the Dynexcel team.
 ###############################################################################
 {
-    'name': "Enrollment",
+    'name': "Openrol - Enrollment",
 
     'summary': """
-    Student Enrollment
+    Simplify, Organize, and Succeed in Enrollment Management
         """,
     'description': """
-        Enrollment
+School Managmeent - Enrollment App
+================================
+Features:
+
+ - Enrollment Order: Seamlessly manage enrollment orders from submission to confirmation.
+ - Student Management: Easily maintain and update student information throughout the enrollment process.
+ - Fee Invoicing: Generate and manage fee invoices for enrolled students.
+ - Pre-defined Fee Templates: Use pre-defined templates to streamline fee structure and invoicing.
+ - Contract Workflow: Efficiently handle contract processes and approvals related to enrollment.
+ - Enrollment Analysis: Analyze enrollment data to gain insights and optimize future enrollment strategies.
     """,
     'author': "Dynexcel",
     'website': "https://www.dynexcel.com",
@@ -37,7 +46,9 @@
            'de_school_enrollment/static/src/js/sale_order_hide_button.js',
        ],
     },
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'license': 'LGPL-3',
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
