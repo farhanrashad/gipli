4432d42c63a9efb2ebaf2bc22c766770ebc18a49
farhan <farhan@dynexcel.com>
2024-04-22 17:12:49 +0000

    update builder

--- de_report_builder/models/report.py
+++ de_report_builder/models/report.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, exceptions, api, _
-from odoo.exceptions import ValidationError, AccessError
+from odoo.exceptions import ValidationError, AccessError, UserError
    
 
 class ReportConfig(models.Model):
@@ -24,7 +24,7 @@ class HeaderFields(models.Model):
     report_config_id = fields.Many2one('report.config', string='Report Config', required=True, ondelete='cascade', index=True, copy=False)
     rc_header_model_id = fields.Many2one(related='report_config_id.rc_header_model_id')
     field_id = fields.Many2one('ir.model.fields', string='Field', ondelete="cascade", required=True,
-                               domain="[('model_id','=',rc_header_mmodel_id)]"
+                               domain="[('model_id','=',rc_header_model_id)]"
                               )
     
 
@@ -35,10 +35,23 @@ class LineModels(models.Model):
 
     report_config_id = fields.Many2one('report.config', string='Report Config', required=True, ondelete='cascade', index=True, copy=False)
 
-    rc_header_model_relations = fields.Many2one('ir.model', ondelete='cascade', string='Line Item Model', store=True)
-    rc_line_model_id = fields.Many2one('ir.model', ondelete='cascade', string='Line Item Model', store=True)
+    rc_header_model_relations = fields.Many2many('ir.model', string='Line Item Model',
+                                                compute='_compute_header_model_relation'
+                                                )
+    rc_line_model_id = fields.Many2one('ir.model', ondelete='cascade', string='Line Item Model', 
+                                       store=True,
+                                       domain="[('id','in',rc_header_model_relations)]"
+                                      )
 
 
+    @api.depends('report_config_id')
+    def _compute_header_model_relation(self):
+        model_id = self.env['ir.model']
+        for record in self:
+            model_ids = self.env['ir.model'].search([
+                ('model','in',record.report_config_id.rc_header_model_id.field_id.mapped('relation'))
+            ])
+            record.rc_header_model_relations = model_ids
     
 
 
