3020e1b58678d127cbf98cd4bac4de75e7ecfecf
farhan <farhan@dynexcel.com>
2024-04-23 00:20:22 +0000

    udpate builder

--- de_report_builder/models/report_line.py
+++ de_report_builder/models/report_line.py
@@ -16,10 +16,37 @@ class LineModels(models.Model):
                                        store=True,
                                        domain="[('id','in',rc_header_model_relations)]"
                                       )
+    rc_header_rel_field_ids = fields.Many2many('ir.model.fields', string="Related Fields", 
+                                             compute='_compute_all_header_rel_fields'
+                                              )
+    rc_header_rel_field_id = fields.Many2one('ir.model.fields', string="Related Field", 
+                                               store=True, readonly=False,
+                                             domain="[('id','in',rc_header_rel_field_ids)]",
+                                             compute='_compute_header_rel_field'
+                                             )
 
     rc_line_model_field_ids = fields.One2many('rc.line.models.fields', 'line_model_id', string='Line Model Fields', copy=True, auto_join=True)
 
-
+    @api.depends('rc_line_model_id')
+    @api.onchange('rc_line_model_id')
+    def _compute_all_header_rel_fields(self):
+        model_ids = self.env['ir.model.fields'].search([
+            ('model_id','=',self.rc_line_model_id.id),
+            ('relation','=',self.report_config_id.rc_header_model_id.model)
+        ])
+        self.rc_header_rel_field_ids = model_ids
+        
+    @api.depends('rc_header_rel_field_ids')
+    def _compute_header_rel_field(self):
+        for record in self:
+            if record.rc_header_rel_field_ids:
+                record.rc_header_rel_field_id = record.rc_header_rel_field_ids[0]
+            else:
+                record.rc_header_rel_field_id = False
+            
+        
+    
+        
     @api.depends('report_config_id')
     def _compute_header_model_relation(self):
         for record in self:
@@ -56,7 +83,7 @@ class LineModelsFields(models.Model):
     _name = 'rc.line.models.fields'
     _description = 'Custom Report Header Fields'
 
-    line_model_id = fields.Many2one('rc.line.models.fields', string='Line Model', required=True, ondelete='cascade', index=True, copy=False)
+    line_model_id = fields.Many2one('rc.line.models', string='Line Model', required=True, ondelete='cascade', index=True, copy=False)
     rc_line_model_id = fields.Many2one(related='line_model_id.rc_line_model_id')
     field_id = fields.Many2one('ir.model.fields', string='Field', ondelete="cascade", required=True,
                                domain="[('model_id','=',rc_line_model_id),('ttype','not in',['one2many'])]"
--- de_report_builder/wizards/report_wizard.py
+++ de_report_builder/wizards/report_wizard.py
@@ -14,10 +14,11 @@ class ReportWizard(models.TransientModel):
         #self._generate_output(report_id)
         #raise UserError(self._generate_output(report_id))
         html_data = """
+            <div class="page">
             <div class="text-center" style="break-inside: avoid;">
                 <h2>""" + report_id.name + """</h2>
             </div>
-        """ + self._generate_output(report_id)
+        """ + self._generate_output(report_id) + """</div>"""
         data = {
             'date_start': False, 
             'date_stop': False, 
@@ -25,14 +26,80 @@ class ReportWizard(models.TransientModel):
         }
         return self.env.ref('de_report_builder.action_custom_report').report_action([], data=data)
 
-    def _generate_output(self,report_id):
+    def _generate_output(self, report_id):
         output = ''
-        
+        records = self.env[report_id.rc_header_model_id.model].search([])
+        test = []
         if report_id.rc_line_model_ids:
-            pass
+            for record in records:
+                for line_model in report_id.rc_line_model_ids:
+                    field_name = line_model.rc_header_rel_field_id.name
+                    lines = self.env[line_model.rc_line_model_id.model].search([(field_name, '=', record.id)])
+                    lines_fields = line_model.rc_line_model_field_ids
+                    output += self._generate_table_output(lines, lines_fields)
+                output += """<div style="page-break-after: always;"/>"""
         else:
+            lines = records
+            lines_fields = report_id.rc_header_field_ids
+            output += self._generate_table_output(lines, lines_fields)
+    
+        return output
+    
+    def _generate_table_output(self, records, fields):
+        output = ''
+        output += """<table class="table table-sm">"""
+        output += """<thead>"""
+        output += """<tr>"""
+        for field in fields:
+            output += """<th class="text-start">""" + str(field.field_id.field_description) + """</th>"""
+        output += """</tr>"""
+        output += """</thead>"""
+        output += """<tbody>"""
+        output += self._generate_output_values(records, fields)
+        output += """</tbody>"""
+        output += """</table>"""
+        
+        return output
+    
+    def _generate_output_values(self, records, fields):
+        output = ''
+        for record in records:
+            output += """<tr>"""
+            for field in fields:
+                if record[field.field_id.name]:
+                    if field.field_id.ttype == 'many2one':
+                        related_record = record[field.field_id.name]
+                        if related_record and hasattr(related_record, field.link_field_id.name):
+                            link_field_value = getattr(related_record, field.link_field_id.name)
+                            output += """<td class="text-start">""" + str(link_field_value) + """</td>"""
+                    elif field.field_id.ttype == 'many2many':
+                        related_records = record[field.field_id.name]
+                        if related_records:
+                            display_names = ", ".join(str(r[field.link_field_id.name]) for r in related_records)
+                            output += """<td class="text-start">""" + display_names + """</td>"""
+                    else:
+                        output += """<td class="text-start">""" + str(record[field.field_id.name]) + """</td>"""
+                else:
+                    output += """<td class="text-start"/>"""
+            output += """</tr>"""
+        return output
+
+
+            
+    
+    def _generate_output1111(self,report_id):
+        output = ''
+        self._generate_lineitems_output(report_id)
+        if report_id.rc_line_model_ids:
+            header_model_id = report_id.rc_header_model_id
+
+            for line in report_id.rc_line_model_ids:
+                field_lines = line.rc_line_model_field_ids
+                line_model_id = line.rc_line_model_id
+        else:
+            header_model_id = False
             field_lines = report_id.rc_header_field_ids
-            model_id = report_id.rc_header_model_id
+            line_model_id = report_id.rc_header_model_id
 
         output += """
             <table class="table table-sm">
@@ -52,7 +119,7 @@ class ReportWizard(models.TransientModel):
                 <tbody>
         """
         
-        output += self._generate_lineitems_output(model_id, field_lines)
+        output += self._generate_lineitems_output(header_model_id, line_model_id, field_lines)
         output += """
                 </tbody>
         """
@@ -62,6 +129,7 @@ class ReportWizard(models.TransientModel):
         """
         return output
 
+    
     def _generate_heading_ouput(self,field_lines):
         output = ''
         for line in field_lines:
@@ -69,8 +137,28 @@ class ReportWizard(models.TransientModel):
             <th class="text-start">""" + str(line.field_id.field_description) + """</th>
             """
         return output
-    def _generate_lineitems_output(self, model_id, field_lines):
+        
+    def _generate_lineitems_output(self, report_id):
         output = ''
+        
+        records = self.env[report_id.rc_header_model_id.model].search([
+            
+        ])
+        for record in records:
+            for line_model in report_id.rc_line_model_ids:
+                field_name = line_model.rc_header_rel_field_id.name
+                lines = self.env[line_model.rc_line_model_id.model].search([(field_name, '=', record.id)])
+                lines_fields = line_model.rc_line_model_field_ids
+        return output
+        
+    def _generate_lineitems_output1111(self, header_model_id, line_model_id, field_lines):
+        output = ''
+        #raise UserError(model_id)
+        if header_model_id:
+            records = self.env[header_model_id.model].search([
+            ])
+            #for reocrd in records:
+            #    liens = 
         records = self.env[model_id.model].search([
             
         ])
@@ -89,7 +177,7 @@ class ReportWizard(models.TransientModel):
                             display_names = ", ".join(str(r[line.link_field_id.name]) for r in related_records)
                             output += """<td class="text-start">""" + display_names + """</td>"""
                     else:
-                        output += """<td class="text-start">""" + record[line.field_id.name] + """</td>"""
+                        output += """<td class="text-start">""" + str(record[line.field_id.name]) + """</td>"""
                 else:
                     output += """<td class="text-start"/>"""
             output += """</tr>"""
