e53097f72454115eead4a041c6deb981916b4b0c
farhan <farhan@dynexcel.com>
2024-05-21 18:29:20 +0000

    update library"

--- de_school_library/__manifest__.py
+++ de_school_library/__manifest__.py
@@ -1,28 +1,33 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Library",
+    'name': "Openrol - Library",
 
     'summary': """
-    Library Management
+    Empowering Education Through Efficient Library Management
         """,
 
     'description': """
-        Library Management
-    """,
+School Managmeent - Library App
+================================
 
+Features:
+ - User-friendly interface: Intuitive and easy-to-navigate dashboard for students, teachers, and librarians.
+ - Catalog management: Efficient organization and management of books, journals, and multimedia resources; advanced search and filter options to locate items quickly.
+ - Circulation management: Streamlined check-in and check-out processes; automated due date reminders and overdue notifications.
+ - User management: Student, teacher, and staff profiles with personalized access and borrowing limits; role-based access control.
+ - Inventory management: Real-time tracking of inventory levels and status updates; tools for adding, updating, and deleting library items.
+ - Reservation system: Ability for users to reserve books and resources online; notification system for available reservations.
+    """,
     'author': "Dynexcel",
     'website': "https://www.dynexcel.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Sales/Library',
-    'version': '0.2',
-
-    # any module necessary for this one to work correctly
-    'depends': ['de_school','sale','stock','web'],
-
-    # always loaded
+    'category': 'CRM/Sale/Industries',
+    'version': '17.0.0.2',
+    'depends': [
+        'de_school',
+        'sale',
+        'stock',
+        'web'
+    ],
     'data': [
         'security/library_security.xml',
         'security/ir.model.access.csv',
@@ -39,16 +44,21 @@
         'views/teacher_views.xml',
         'views/agreement_views.xml',
         'wizards/fee_configurator_views.xml',
-        'reports/report_library_views.xml',
+        #'reports/report_library_views.xml',
     ],
     'js': [
-        'de_school_library/static/src/js/library_fee_config_wizard.js',
+        #'de_school_library/static/src/js/library_fee_config_wizard.js',
     ],
     'assets': {
        'web.assets_backend': [
            #'de_school_library/static/src/js/library_fee_config_wizard.js',
            #'de_school_library/static/src/js/library_fee_config_wizard.js',
-           'de_school_library/static/src/**/*',
+           #'de_school_library/static/src/**/*',
        ],
     },
+    'license': 'LGPL-3',
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
--- de_school_library/models/sale_order_line.py
+++ de_school_library/models/sale_order_line.py
@@ -6,6 +6,8 @@ from pytz import timezone, UTC
 
 from odoo import api, fields, models, _
 from odoo.tools import format_datetime, format_time
+from odoo.exceptions import UserError
+from dateutil.relativedelta import relativedelta
 
 
 class SaleOrderLine(models.Model):
@@ -13,13 +15,29 @@ class SaleOrderLine(models.Model):
 
     is_borrow_order = fields.Boolean(related='order_id.is_borrow_order')
     is_product_book = fields.Boolean(related='product_id.is_book', depends=['product_id'])
-
+    borrow_status = fields.Selection(related='order_id.borrow_status')
 
     book_pickup_date = fields.Datetime(
         string="Pickup", )
     book_return_date = fields.Datetime(
         string="Return", )
 
+    book_issue_date = fields.Datetime(string='Issue Date',
+            default=lambda s: fields.Datetime.now() + relativedelta(minute=0, second=0, hours=1))
+    book_return_date = fields.Datetime(string="Return Date",
+            default=lambda s: fields.Datetime.now() + relativedelta(minute=0, second=0, hours=1, days=1))
+    duration = fields.Integer(
+        string="Duration", compute="_compute_book_duration",
+        help="The duration unit is based on the unit of the rental pricing rule.")
+    duration_unit = fields.Selection([("hour", "Hours"), ("day", "Days"), ("week", "Weeks"), ("month", "Months"), ('year', "Years")],
+                                     string="Unit", required=True, compute="_compute_book_duration")
+
+    book_pricing_id = fields.Many2one('lib.product.fees', compute="_compute_book_pricing",
+        string="Pricing", help="Best Pricing Rule based on duration")
+
+    pricelist_id = fields.Many2one('product.pricelist', string='Pricelist')
+    book_qty_returned = fields.Float("Returned", default=0.0, copy=False)
+    
     @api.onchange('product_id')
     def _onchange_product_id(self):
         """Clean rental related data if new product cannot be rented."""
