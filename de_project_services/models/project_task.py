b5ff34de0996fb0e104ffc25dc14ef25345b483f
farhan <farhan@dynexcel.com>
2024-06-18 22:17:26 +0000

    update services

--- de_project_services/models/project_task.py
+++ de_project_services/models/project_task.py
@@ -9,3 +9,10 @@ class ProjecTask(models.Model):
     is_service_project_template = fields.Boolean(related='project_id.is_service_project_template')
     days_completion = fields.Integer(string='Days Completion')
 
+    service_product_id = fields.Many2one(
+        'product.product', 
+        string='Service Product', 
+        domain=[('type', '=', 'service')]
+    )
+    
+
