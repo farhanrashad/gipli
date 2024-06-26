cca672e8898a3f2fb1e86c05080ee652a9091c7c
farhan <farhan@dynexcel.com>
2024-06-26 21:04:54 +0000

    update hr service

--- de_portal_hr_service/controllers/main.py
+++ de_portal_hr_service/controllers/main.py
@@ -534,7 +534,7 @@ class CustomerPortal(CustomerPortal):
             </script>
         '''
         user_avatar = f"/web/image/res.partner/{request.env.user.partner_id.id}/avatar_128"
-        user_ids = request.env['res.users'].search([('active','=',True)])
+        user_ids = request.env['res.users'].sudo().search([('active','=',True)])
 
         user_html = '''
         <label for="user_ids">Mentions</label>
