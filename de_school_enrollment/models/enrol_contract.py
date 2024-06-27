80819a0c4df96660352f666363d0953e16ccb8c8
farhan <farhan@dynexcel.com>
2024-06-27 15:32:27 +0000

    udpate enrollment

--- de_school_enrollment/models/enrol_contract.py
+++ de_school_enrollment/models/enrol_contract.py
@@ -71,7 +71,7 @@ class EnrollmentContract(models.Model):
     def _compute_admission_team_id(self):
         cached_teams = {}
         for order in self:
-            default_team_id = order.admission_team_id.id
+            default_team_id = order.admission_team_id.id if order.admission_team_id else None
             user_id = order.user_id.id
             company_id = order.company_id.id
             key = (default_team_id, user_id, company_id)
