f7e1513827627c7cff983b1b7ef19fb873646fd3
Farhan <farhan@xpendless.com>
2024-07-14 21:30:48 +0500

    Update __manifest__.py

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'odoo 16 Customize OEM(Boost, Data reset)',
-    'version': '17.23.03.09',
+    'version': '17.0.0.2',
     'author': 'Sunpop.cn',
     'category': 'Productivity',
     'website': 'https://www.sunpop.cn',
