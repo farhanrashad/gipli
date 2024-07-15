ddd2e7fce0ca7518acdbe3a6f8350647df738916
farhan <farhan@dynexcel.com>
2024-07-15 20:04:02 +0000

    udpate helpdess

--- de_helpdesk_website/__manifest__.py
+++ de_helpdesk_website/__manifest__.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Website Helpdesk",
-
     'summary': "This module enables website users to generate helpdesk tickets directly.",
-
     'description': """
 This module allows users to easily generate helpdesk tickets directly from the website, streamlining the support process. It integrates seamlessly with existing helpdesk systems, ensuring efficient ticket management and prompt responses to user inquiries.
 """,
-
     'author': "Dynexcel",
     'website': "https://www.dynexcel.com",
     'category': 'Services/Helpdesk',
@@ -21,5 +18,12 @@ This module allows users to easily generate helpdesk tickets directly from the w
         'views/helpdesk_project_views.xml',
         'views/helpdesk_portal_templates.xml',
     ],
+    'license': 'OPL-1',
+    'price': 10,
+    'currency': 'USD',
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
 
