8fbfa7d219028c7192a9679f414f1ddb97c2ed3f
farhan <farhan@dynexcel.com>
2024-07-15 15:05:19 +0000

    udpate helpdesk

--- de_helpdesk_website/controllers/controllers.py
+++ de_helpdesk_website/controllers/controllers.py
@@ -3,11 +3,36 @@
 
 from odoo import http, _
 from odoo.http import request
+from odoo.exceptions import UserError, ValidationError
 
 
 class WebsiteHelpdesk(http.Controller):
 
-    @http.route(['/support'], type="http", auth="public", website=True, sitemap=True)
-    def faq_ans(self, **kw):
-        obj = request.env["project.project"].search([('is_published', '=', True)])
-        return request.render("de_helpdesk_website.web_faq", {"object": obj, })
+    
+    #@http.route(['/support/'], type="http", auth="public", website=True, sitemap=True)
+    #def faq_ans(self, **kw):
+    #    obj = request.env["project.project"].search([('is_published', '=', True)])
+    #    return request.render("de_helpdesk_website.web_faq", {"object": obj, })
+
+    @http.route(['/support/<string:url>'], type="http", auth="public", website=True)
+    def website_helpdesk(self, url, **kw):
+        # Search for the project with the given URL
+        #raise UserError('/support/'+url)
+        project = request.env["project.project"].sudo()._get_project_by_website_url('/support/'+url)
+        #search([('website_url', '=', 'support/'+url)], limit=1)
+        
+        if not project:
+            return request.not_found()
+
+        vals = {
+            'project': project,
+        }
+        
+        return request.render("de_helpdesk_website.website_helpdesk_form", vals)
+
+
+    @http.route('/support/ticket/submit', type="http", website=True, auth='public', csrf=False)
+    def ticket_submit(self, **kw):
+        ticket = request.env['project.task'].browse(1)
+        return request.render("de_helpdesk_website.support_ticket_submited", {"ticket": ticket})
+        
--- de_helpdesk_website/models/project.py
+++ de_helpdesk_website/models/project.py
@@ -10,10 +10,12 @@ from odoo.addons.http_routing.models.ir_http import slug
 class Project(models.Model):
     _inherit = 'project.project'
 
-    is_published = fields.Boolean("Web Published", compute='_compute_is_published')
+    is_published = fields.Boolean("Web Published", store=True,
+                                  compute='_compute_is_published')
 
     website_id = fields.Many2one('website', domain="[('company_id', '=?', company_id)]", compute='_compute_website_id', store=True, readonly=False)
-    website_url = fields.Char('Url', compute='_compute_website_url',)
+    website_url = fields.Char('Url', store=True,
+                              compute='_compute_website_url',)
 
     @api.depends('module_helpdesk_website')
     def _compute_is_published(self):
@@ -22,12 +24,21 @@ class Project(models.Model):
             
     @api.depends('company_id')
     def _compute_website_id(self):
-        for team in self:
-            team.website_id = team.company_id.website_id
+        for record in self:
+            record.website_id = record.company_id.website_id
 
+    @api.depends('is_published')
     def _compute_website_url(self):
-        for team in self:
-            team.website_url = "/support/%s" % slug(team)
+        for record in self:
+            record.website_url = "/support/%s" % slug(record)
             
     def open_website_url(self):
+        #self._get_project_by_website_url('/support/support-desk-5')
         return self.env['website'].get_client_action(self.website_url, website_id=self.website_id.id)
+
+    def _get_project_by_website_url(self, url):
+        #raise UserError(url)
+        project = self.env["project.project"].search([
+            ('website_url', '=', url)
+        ], limit=10)
+        return project
