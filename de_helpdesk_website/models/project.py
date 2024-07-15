407b6e6ab4d6708c5a1ef382d53f06bd901f1107
farhan <farhan@dynexcel.com>
2024-07-15 10:25:46 +0000

    update helpdesk website

--- de_helpdesk_website/models/project.py
+++ de_helpdesk_website/models/project.py
@@ -10,11 +10,16 @@ from odoo.addons.http_routing.models.ir_http import slug
 class Project(models.Model):
     _inherit = 'project.project'
 
-    is_published = fields.Boolean("Web Published")
+    is_published = fields.Boolean("Web Published", compute='_compute_is_published')
 
     website_id = fields.Many2one('website', domain="[('company_id', '=?', company_id)]", compute='_compute_website_id', store=True, readonly=False)
     website_url = fields.Char('Url', compute='_compute_website_url',)
 
+    @api.depends('module_helpdesk_website')
+    def _compute_is_published(self):
+        for record in self:
+            record.is_published = record.module_helpdesk_website
+            
     @api.depends('company_id')
     def _compute_website_id(self):
         for team in self:
@@ -22,7 +27,7 @@ class Project(models.Model):
 
     def _compute_website_url(self):
         for team in self:
-            team.website_url = "/helpdesk/%s" % slug(team)
+            team.website_url = "/support/%s" % slug(team)
             
     def open_website_url(self):
         return self.env['website'].get_client_action(self.website_url, website_id=self.website_id.id)
