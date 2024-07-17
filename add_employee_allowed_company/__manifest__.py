c3f32ea06725d3a4aedcc75a545f176e71b52db5
Farhan <farhan@xpendless.com>
2024-07-17 18:44:20 +0500

    add module

---
+++ add_employee_allowed_company/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "add_change_employee_company",
+    'depends': ['base','hr','hr_contract','centione_hr_end_service'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+
+}
