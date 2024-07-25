55c705cb7e1f75f92f9d8922180ea13c657cc0bc
farhan <farhan@dynexcel.com>
2024-07-25 06:43:07 +0000

    udpate service

--- de_portal_hr_service/__manifest__.py
+++ de_portal_hr_service/__manifest__.py
@@ -12,7 +12,7 @@
     'author': "dynexcel",
     'website': "https://www.dynexcel.com",
     'category': 'Human Resources',
-    'version': '16.0.6.9',
+    'version': '16.0.7.1',
     'license': 'LGPL-3',
     'live_test_url': 'https://youtu.be/T3ZI7eh0qbg',
     'price': 149,
