5b6c77e2dc9c90513dd1dca337053d476bd559a4
farhan <farhan@dynexcel.com>
2024-07-29 22:10:40 +0000

    fix

--- de_portal_dashboard/controllers/portal-Copy1.py
+++ de_portal_dashboard/controllers/portal-Copy1.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-import plotly.express as px
-import json
 from odoo import http
 from odoo.http import request
-
 from odoo.addons.portal.controllers.portal import CustomerPortal
 
 class CustomerPortal(CustomerPortal):
@@ -12,7 +8,7 @@ class CustomerPortal(CustomerPortal):
     @http.route('/my/services/dashboard', type='http', auth='public', website=True, csrf=False)
     def my_services_dashboard(self, **kwargs):
         dash_item_ids = request.env['base.dashboard.item'].search([])
-        
+
         output_html = '''
             <div class="container">
                 <h1>Services Dashboard</h1>
@@ -20,11 +16,47 @@ class CustomerPortal(CustomerPortal):
         '''
         
         for item in dash_item_ids:
-            if item.view_type == 'list':
-                output_html += self.render_list_view(item)
-            elif item.view_type == 'graph':
-                output_html += self.render_graph_view(item)
-        
+            records = request.env[item.model_id.model].search([])
+            output_html += '''
+                <div class="col-lg-6">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">{item_name}</h5>
+                            <table id="table-{item_id}" class="display">
+                                <thead>
+                                    <tr>
+            '''.format(item_name=item.name, item_id=item.id)
+
+            # Adding table headers
+            for field in item.field_ids:
+                output_html += '<th>{}</th>'.format(field.name.capitalize())
+                
+            output_html += '''
+                                    </tr>
+                                </thead>
+                                <tbody>
+            '''
+            
+            # Adding table rows with links to record details
+            for record in records:
+                output_html += '<tr>'
+                for field in item.field_ids:
+                    record_value = getattr(record, field.name, '')
+                    output_html += '<td><a href="/my/services/record/{model_id}/{record_id}">{record_value}</a></td>'.format(
+                        model_id=item.model_id.id,
+                        record_id=record.id,
+                        record_value=record_value
+                    )
+                output_html += '</tr>'
+
+            output_html += '''
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            '''
+
         output_html += '''
                 </div>
             </div>
@@ -67,95 +99,11 @@ class CustomerPortal(CustomerPortal):
                 });
             </script>
         '''
-    
+
         values = {
             'output_html': output_html,
         }
         return request.render("de_portal_dashboard.portal_services_dashboard", values)
-    
-    def render_list_view(self, item):
-        records = request.env[item.model_id.model].search([])
-        
-        list_view_html = '''
-            <div class="col-lg-6">
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <h5 class="card-title">{item_name}</h5>
-                        <table id="table-{item_id}" class="display">
-                            <thead>
-                                <tr>
-        '''.format(item_name=item.name, item_id=item.id)
-        
-        # Adding table headers
-        for field in item.field_ids:
-            list_view_html += '<th>{}</th>'.format(field.name.capitalize())
-        
-        list_view_html += '''
-                            </tr>
-                        </thead>
-                        <tbody>
-        '''
-        
-        # Adding table rows with links to record details
-        for record in records:
-            list_view_html += '<tr>'
-            for field in item.field_ids:
-                record_value = getattr(record, field.name, '')
-                list_view_html += '<td><a href="/my/services/record/{model_id}/{record_id}">{record_value}</a></td>'.format(
-                    model_id=item.model_id.id,
-                    record_id=record.id,
-                    record_value=record_value
-                )
-            list_view_html += '</tr>'
-        
-        list_view_html += '''
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        '''
-        
-        return list_view_html
-    
-
-    def render_graph_view(self, item):
-        # Fetch data from the model
-        records = request.env[item.model_id.model].search([])
-    
-        # Prepare data using pandas
-        data = []
-        for record in records:
-            label = getattr(record, item.graph_label_field_id.name, 'No Label')
-            value = getattr(record, item.graph_data_field_id.name, 0)
-            data.append({'label': label, 'value': value})
-    
-        # Create a DataFrame
-        df = pd.DataFrame(data)
-    
-        # Create the Plotly figure
-        fig = px.bar(df, x='label', y='value', title="Graph Title", color='label')
-    
-        # Convert Plotly figure to HTML
-        graph_html = fig.to_html(full_html=False)
-    
-        # Wrap the Plotly HTML into a div container
-        graph_view_html = (
-            '<div class="col-lg-6">'
-            '    <div class="card mb-3">'
-            '        <div class="card-body">'
-            '            <h5 class="card-title">' + item.name + '</h5>'
-            '            <div id="graph-' + str(item.id) + '">' + graph_html + '</div>'
-            '        </div>'
-            '    </div>'
-            '</div>'
-        )
-    
-        return graph_view_html
-
-
-
-
-
 
     @http.route('/my/services/record/<int:model_id>/<int:record_id>', type='http', auth='public', website=True, csrf=False)
     def record_detail(self, model_id, record_id, **kwargs):
--- de_portal_dashboard/models/dashboard.py
+++ de_portal_dashboard/models/dashboard.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api, _
-
 
 class BaseDashboardItem(models.Model):
     _name = 'base.dashboard.item'
     _description = 'Dashboard Item'
+    _order = 'id'
 
     name = fields.Char(string='Name', required=True, translate=True)
     model_id = fields.Many2one('ir.model', ondelete='cascade', string='Model', required=True)
     field_ids = fields.Many2many(
         'ir.model.fields', 
         string='Fields',
-        domain="[('model_id', '=', model_id)]"
+        domain="[('model_id', '=', model_id), ('ttype', 'in', ['integer', 'float', 'monetary'])]"
+    )
+    view_type = fields.Selection([
+        ('list', 'List'),
+        ('graph', 'Graph')
+    ], string='View Type', required=True, default='list')
+    graph_label_field_id = fields.Many2one(
+        'ir.model.fields', 
+        string='Label Field',
+        domain="[('model_id', '=', model_id), ('ttype', 'in', ['char', 'selection'])]"
+    )
+    graph_data_field_id = fields.Many2one(
+        'ir.model.fields', 
+        string='Data Field',
+        domain="[('model_id', '=', model_id), ('ttype', 'in', ['integer', 'float', 'monetary'])]"
     )
