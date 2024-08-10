9ee55a5bab37157f5665eb19deebd9a756375ee6
farhan <farhan@dynexcel.com>
2024-08-10 08:45:23 +0000

    update tax

--- de_tax_pk/models/account_move.py
+++ de_tax_pk/models/account_move.py
@@ -28,10 +28,8 @@ class AccountMove(models.Model):
     )
 
     @api.depends(
-        'line_ids.matched_debit_ids.debit_move_id.move_id.payment_id.is_matched',
         'line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual',
         'line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual_currency',
-        'line_ids.matched_credit_ids.credit_move_id.move_id.payment_id.is_matched',
         'line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual',
         'line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual_currency',
         'line_ids.balance',
@@ -39,7 +37,6 @@ class AccountMove(models.Model):
         'line_ids.amount_currency',
         'line_ids.amount_residual',
         'line_ids.amount_residual_currency',
-        'line_ids.payment_id.state',
         'line_ids.full_reconcile_id',
         'state')
     def _compute_gst_wht_amount(self):
